386fadabd91ef2a3171f8eff1e586951568c63a3
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-07-19 21:40:15 -0500

    Time: 82 ms (90.08%), Space: 71.4 MB (65.89%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -0,0 +1,48 @@
+class wordIndexPair{
+    String word;
+    int index;
+    wordIndexPair(String word){
+        this.word=word;
+        index=0;
+    }
+}
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        List<Queue<wordIndexPair>> wordIndexPairList=new ArrayList<>();
+
+        for(int i=0;i<26;i++){
+            wordIndexPairList.add(new LinkedList<>());
+        }
+        
+        for(String word:words){
+            wordIndexPair t=new wordIndexPair(word);
+            t.word=word;
+            t.index=0;
+            wordIndexPairList.get(word.charAt(0)-'a').add(t);
+        }
+        
+        int matchedWordCount=0;
+        for(char ch:s.toCharArray()){
+            Queue<wordIndexPair> curCharQueue=wordIndexPairList.get(ch-'a');
+            int curSize=curCharQueue.size()-1;
+            while(curSize-->=0){
+
+                wordIndexPair cur=curCharQueue.peek();
+                String curWordString=cur.word;
+                int index=cur.index;
+                if(cur.word.charAt(index)==ch){
+                    cur.index++;
+                }
+                if(cur.index==cur.word.length()){
+                    matchedWordCount++;
+                    // wordIndexPairList.remove
+                }else{
+                    wordIndexPairList.get(cur.word.charAt(cur.index)-'a').add(cur);
+                }
+                
+                curCharQueue.poll();
+            }
+        }
+        return matchedWordCount;
+    }
+}
