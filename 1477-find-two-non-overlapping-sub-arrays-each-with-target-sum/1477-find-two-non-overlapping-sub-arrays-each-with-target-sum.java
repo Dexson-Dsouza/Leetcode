f702633132710cd6b5c4d76c623bbbabd8b5ae56
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-07-22 07:49:16 -0500

    Time: 391 ms (5.15%), Space: 197.4 MB (5.82%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,42 +1,37 @@
 class Solution {
-    public int minSumOfLengths(int[] arr, int target) {
-        int result=Integer.MAX_VALUE;
-        int prevSmallest=Integer.MAX_VALUE;
-        
-        Queue<int[]> prevValidSubArrays=new LinkedList<>();
-        
-        int startPointer=0;
-        int endPointer=0;
-        int curSum=0;
-        int inputLength=arr.length;
-        
-        while(endPointer<inputLength){
-            
-            // System.out.println(startPointer+" "+endPointer+" "+prevSmallest+" "+curSum);
-            
-            curSum+=arr[endPointer];
-            
-            
-            while(curSum>=target){
-                if(curSum==target){
-                    int curLen=endPointer-startPointer+1;
-                    // System.out.println(curLen+" "+prevSmallest+"___"+startPointer+" "+endPointer);
-                    prevValidSubArrays.add(new int[]{endPointer,curLen});
-                    if(prevSmallest!=Integer.MAX_VALUE){
-                        result=Math.min(result,prevSmallest+curLen);
-                    }
-                    
-                }
-                curSum-=arr[startPointer];
-                startPointer++;
-                while(prevValidSubArrays.size()>0 && prevValidSubArrays.peek()[0]<startPointer){
-                    prevSmallest=Math.min(prevSmallest,prevValidSubArrays.poll()[1]);
-                }
-            }
-            endPointer++;
-            
-            
+   public int minSumOfLengths(int[] arr, int target) {          
+    int[] left = new int[arr.length];
+    int sum = 0, best = Integer.MAX_VALUE;
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0, -1);   
+    for (int i = 0; i < arr.length; ++i) {
+        sum += arr[i];
+        if (map.containsKey(sum - target)) {
+            best = Math.min(best, i - map.get(sum - target));  
         }
-        return result==Integer.MAX_VALUE?-1:result;
+        left[i] = best;
+        map.put(sum, i);    
     }
+    
+    int[] right = new int[arr.length];
+    sum = 0; best = Integer.MAX_VALUE;
+    map = new HashMap<>();
+    map.put(0, arr.length);    
+    for (int i = arr.length - 1; i >= 0; --i) {
+        sum += arr[i];
+        if (map.containsKey(sum - target)) {
+            best = Math.min(best, map.get(sum - target) - i); 
+        }
+        right[i] = best;
+        map.put(sum, i);  
+    }
+    
+    int result = Integer.MAX_VALUE;
+    for (int i = 1; i < arr.length; ++i) {
+        if (left[i - 1] != Integer.MAX_VALUE && right[i] != Integer.MAX_VALUE) {
+            result = Math.min(result, left[i - 1] + right[i]);  
+        }
+    }
+    return result == Integer.MAX_VALUE ? -1 : result;
+}
 }
