1ce47986dd48ecd6175b6b7a65f99811785ffd1e
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-07-23 21:36:50 -0500

    Time: 1 ms (97.98%), Space: 42.7 MB (12.53%) - LeetHub

--- 593-valid-square/593-valid-square.java
+++ 593-valid-square/593-valid-square.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
-        return isValid(p1,p2,p3,p4) ||isValid(p1,p3,p2,p4) ||isValid(p1,p2,p4,p3) || 
-            isValid(p1,p3,p4,p2) ||isValid(p1,p4,p2,p3) ||isValid(p1,p4,p3,p2);
+        return isValid(p1,p2,p3,p4) ||isValid(p1,p3,p2,p4) ||isValid(p1,p2,p4,p3);
     }
     
     boolean isValid(int[] p1, int[] p2, int[] p3, int[] p4){
