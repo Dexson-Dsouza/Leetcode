30d1e42d1a89aa1565509b46afa990f26ca848fc
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-07-24 16:08:13 -0500

    Time: 63 ms (64.40%), Space: 72.2 MB (28.12%) - LeetHub

---
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canReorderDoubled(int[] arr) {
+    
+        Arrays.sort(arr);
+        Map<Integer,Integer> count=new HashMap<>();
+        for(int n:arr){
+            count.put(n,count.getOrDefault(n,0)+1);
+        }
+        List<Integer> keys=new ArrayList<>(count.keySet());
+        Collections.sort(keys);
+        for(int key:keys){
+            int cur=key;
+            int pair=cur*2;
+            if(count.containsKey(cur)==false || count.containsKey(pair)==false){
+                continue;
+            }
+            if(count.get(cur)<=0 || count.get(pair)<=0){
+                continue;
+            }
+            int minimumCnt=Math.min(count.get(cur),count.get(pair));
+            count.put(cur,count.get(cur)-minimumCnt);
+            
+            count.put(pair,count.get(pair)-minimumCnt);
+        }
+        
+        for(int key:count.keySet()){
+            // System.out.println(key+" "+count.get(key));
+            if(count.get(key)>0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
