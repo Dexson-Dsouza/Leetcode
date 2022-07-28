438618dae5a04f48afa581cc72ecee321785ddc9
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-07-27 20:57:28 -0500

    Time: 49 ms (45.60%), Space: 44.4 MB (28.40%) - LeetHub

--- 664-strange-printer/664-strange-printer.java
+++ 664-strange-printer/664-strange-printer.java
@@ -1,35 +1,26 @@
 class Solution {
     public int strangePrinter(String s) {
-
-        if (s == null || s.length() == 0) {
-            return 0;
+        int len=s.length();
+        
+        int[][] dp=new int[len][len];
+        
+        for(int i=0;i<len;i++){
+            dp[i][i]=1;
         }
-
-        int n = s.length();
-        int[][] state = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            state[i][i] = 1;
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int dist = 1; dist + i < n; dist++) {
-                int j = dist + i;
-                // if (dist == 1) {
-                //     state[i][j] = (s.charAt(i) == s.charAt(j)) ? 1 : 2;
-                //     continue;
-                // }
-                state[i][j] = Integer.MAX_VALUE;
-                for (int k = i; k + 1 <= j; k++) {
-                    int tmp = state[i][k] + state[k + 1][j];
-                    state[i][j] = Math.min(state[i][j], tmp);
-                }
-                if (s.charAt(i) == s.charAt(j)) {
-                    state[i][j]--;
+        
+        for(int curLen=2;curLen<=len;curLen++){
+            for(int start=0;start+curLen-1<len;start++){
+                int end=start+curLen-1;
+                dp[start][end]=Integer.MAX_VALUE;
+                for(int k=start;k<end;k++){
+                    int curTurns= dp[start][k]+dp[k+1][end];
+                    if(s.charAt(start)==s.charAt(end)){
+                        curTurns--;
+                    }
+                    dp[start][end]=Math.min(dp[start][end],curTurns);
                 }
             }
         }
-
-        return state[0][n - 1];
+        return dp[0][len-1];
     }
 }
