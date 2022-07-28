f3e056788257f1ff0be2c566ccb7c187a243a11b
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-07-28 16:32:40 -0500

    Time: 11 ms (40.78%), Space: 43.6 MB (15.58%) - LeetHub

--- 920-number-of-music-playlists/920-number-of-music-playlists.java
+++ 920-number-of-music-playlists/920-number-of-music-playlists.java
@@ -1,17 +1,21 @@
 class Solution {
     public int numMusicPlaylists(int n, int goal, int k) {
         long[][] ways=new long[goal+1][n+1];
-        
-        int mod=(int)(Math.pow(10,9)+7);
         ways[0][0]=1;
-        for(int num=1;num<=goal;num++){
-            for(int song=1;song<=n;song++){
-                ways[num][song] = (ways[num-1][song-1]*(n-(song-1)))%mod;
-                if(song>k){
-                    ways[num][song] = (ways[num][song] + ways[num-1][song]*(song-k))%mod;
+        int mod=(int)(Math.pow(10,9)+7);
+        for(int curGoal=1;curGoal<=goal;curGoal++){
+            for(int choice=1;choice<=n;choice++){
+                ways[curGoal][choice] =( ways[curGoal-1][choice-1] * (n-choice+1))%mod;
+                if(choice>k){
+                    ways[curGoal][choice] = (ways[curGoal][choice]+ ways[curGoal-1][choice]*(choice-k)%mod )%mod;
                 }
             }
         }
-        return (int)ways[goal][n];
+        return (int)(ways[goal][n]%mod);
     }
 }
+
+
+// 1 2 3
+
+// 3 * 2 * 1 + 
