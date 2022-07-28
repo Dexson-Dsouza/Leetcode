766b8547d037db2a14711ec238b1cad31637e5ae
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-07-28 09:04:49 -0500

    Time: 1 ms (87.68%), Space: 41.4 MB (81.59%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,45 +1,40 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> prevString=new Stack<>();
-        Stack<Integer> multiplier=new Stack<>();
+        return decode(s,0,s.length());
+    }
+    
+    public String decode(String s, int start, int end){
+        StringBuilder decodedStr=new StringBuilder();
+        Deque<String> prevStr=new LinkedList<>();
+        Deque<Integer> multiplier=new LinkedList<>();
         
-        StringBuilder currentString=new StringBuilder();
-        for(int index=0;index<s.length();index++){
-            // System.out.println(index+" "+currentString );
-            char ch=s.charAt(index);
-            if(isNum(ch)){
-                int count=0;
-                while(index<s.length() && isNum(s.charAt(index))){
-                    count= count*10 + s.charAt(index)-'0';
-                    index++;
+        while(start<end){
+            if(s.charAt(start)==']'){
+                int repeatCount = multiplier.pollLast();
+                StringBuilder repeatedString=new StringBuilder();
+                for(int i=0;i<repeatCount;i++){
+                    repeatedString.append(decodedStr.toString());
                 }
                 
-                prevString.push(currentString.toString());
-                multiplier.push(count);
-                currentString=new StringBuilder();
-            }else if(ch==']'){
-                int count=multiplier.pop();
-                StringBuilder concatenatedString=new StringBuilder();
-                while(count>0){
-                    concatenatedString.append(currentString.toString());
-                    count--;
+                decodedStr = new StringBuilder(prevStr.pollLast() + repeatedString.toString()); 
+            }else if(isNum(s.charAt(start))){
+                int num=0;
+                while(isNum(s.charAt(start))){
+                    num = num*10 + (s.charAt(start)-'0');
+                    start++;
                 }
-                
-                currentString= new StringBuilder(prevString.pop() + concatenatedString.toString());
+                multiplier.addLast(num);
+                prevStr.addLast(decodedStr.toString());
+                decodedStr=new StringBuilder("");
             }else{
-                currentString.append(s.charAt(index));
+                decodedStr.append(s.charAt(start));
             }
+            start++;
         }
-        
-        return currentString.toString();
+        return decodedStr.toString();
     }
     
-    boolean isNum(char c){
-        return c>='0' && c<='9';
+    boolean isNum(char ch){
+        return ch>='0' && ch<='9';
     }
-    
-    boolean isChar(char c){
-        return c>='a' && c<='z';
-    }
-    
 }
