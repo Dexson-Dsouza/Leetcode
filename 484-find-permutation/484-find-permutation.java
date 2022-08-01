f05b4a7da1edc6681160581c6077f7b1f92f2ae9
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-07-31 20:12:32 -0500

    Time: 6 ms (54.26%), Space: 52 MB (46.81%) - LeetHub

---
+++ 484-find-permutation/484-find-permutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] findPermutation(String s) {
+        int[] perm=new int[s.length()+1];
+        
+        int curIndex=0;
+        for(int i=0;i<=s.length();i++){
+            perm[i]=i+1;
+        }
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='I'){
+                int end=i;
+                while(end>curIndex){
+                    int t=perm[curIndex];
+                    perm[curIndex]=perm[end];
+                    perm[end]=t;
+                    end--;
+                    curIndex++;
+                }
+                curIndex=i+1;
+                
+            }
+        }
+        int end=s.length();
+        while(end>curIndex){
+            int t=perm[curIndex];
+            perm[curIndex]=perm[end];
+            perm[end]=t;
+            end--;
+            curIndex++;
+        }
+        
+        return perm;
+    }
+}
