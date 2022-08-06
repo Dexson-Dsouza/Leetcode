4003df9608ce24f8f725135eaf589628f24b3235
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-08-06 18:01:14 -0500

    Time: 143 ms (5.79%), Space: 68.6 MB (5.26%) - LeetHub

--- 568-maximum-vacation-days/568-maximum-vacation-days.java
+++ 568-maximum-vacation-days/568-maximum-vacation-days.java
@@ -3,23 +3,33 @@ class Solution {
         int n=flights.length;
         int k=days[0].length;
         
-        int[] vacationDay=new int[n];        
+        Integer[][] vacationDay=new Integer[k][n];
         
-        for(int week=k-1;week>=0;week--){
-            int[] curWeekVacationDays=new int[n];
-            for(int i=0;i<n;i++){
-                curWeekVacationDays[i]=vacationDay[i] + days[i][week];
-                for(int j=0;j<n;j++){
-                    if(flights[i][j]==1){
-                        curWeekVacationDays[i]=Math.max(curWeekVacationDays[i],
-                                                     vacationDay[j]+days[j][week]); 
-                    }
-                }   
-                
+        for(int city=0;city<n;city++){
+            if(flights[0][city]==1 || city==0){
+                vacationDay[0][city]=days[city][0];
             }
-            vacationDay=curWeekVacationDays;
         }
         
-        return vacationDay[0];
+        for(int week=1;week<k;week++){
+            for(int i=0;i<n;i++){
+
+                for(int j=0;j<n;j++){
+                    if(vacationDay[week-1][j]!=null && (flights[j][i]==1 || j==i)){
+                        if(vacationDay[week][i]==null){
+                            vacationDay[week][i]=0;
+                        }
+                        vacationDay[week][i]=Math.max(vacationDay[week][i],
+                                                     vacationDay[week-1][j]+days[i][week]); 
+                    }
+                }   
+            }
+        }
+        int result=0;
+        for(int i=0;i<n;i++){
+            if(vacationDay[k-1][i]!=null)  
+                result=Math.max(result,vacationDay[k-1][i]);
+        }
+        return result;
     }
 }
