f86c8820db16856f829338506dc875e79c9a834d
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-08-07 19:00:15 -0500

    Time: 9 ms (27.83%), Space: 43.3 MB (15.58%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         String[] numberPad=new String[]{"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        return generateCombos(0,digits,numberPad,new ArrayList<>());
-    }
-    
-    public List<String> generateCombos(int i,String digits, String[] numberPad, List<String> prevCombos){
-        if(i==digits.length()){
-            return prevCombos;
-        }
-        List<String> combinationList=new ArrayList<>();
-        int numberIndex = digits.charAt(i)-'2';
-        if(prevCombos.size()==0){
-            for(char c:numberPad[numberIndex].toCharArray()){
-                combinationList.add(String.valueOf(c));
-            }
-        }else{
-            for(String s:prevCombos){
+        int index=0;
+        List<String> combinationList;
+        List<String> prevCombos=new ArrayList<>();
+        while(index<digits.length()){
+            combinationList=new ArrayList<>();
+            int numberIndex = digits.charAt(index)-'2';
+            if(prevCombos.size()==0){
                 for(char c:numberPad[numberIndex].toCharArray()){
-                    combinationList.add(s+c);
+                    combinationList.add(String.valueOf(c));
+                }
+            }else{
+                for(String s:prevCombos){
+                    for(char c:numberPad[numberIndex].toCharArray()){
+                        combinationList.add(s+c);
+                    }
                 }
             }
+            prevCombos=combinationList;
+            index++;
         }
-        return generateCombos(i+1,digits,numberPad,combinationList);
+        return prevCombos;
     }
 }
