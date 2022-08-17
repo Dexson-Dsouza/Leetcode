9b0eae7d975e196dffe35bccaabd3a3aa9c70822
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-08-17 14:11:21 -0500

    Time: 11 ms (9.60%), Space: 46.7 MB (7.65%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,50 +1,18 @@
 class Solution {
     public int minMoves2(int[] nums) {
-        int mid=quickSelectMedian(nums,0,nums.length-1,nums.length/2);
-        // System.out.println(mid);
-        int moves=0;
+        Arrays.sort(nums);
+        long curMoves=0;
         for(int n:nums){
-            // System.out.println(n);
-            moves+=Math.abs(n-mid);
+            curMoves+=Math.abs(n-nums[0]);
         }
-        return moves;
-    }
-    
-    int quickSelectMedian(int[] nums, int left, int right, int mid){
-        if(left==right){
-            return nums[left];
+        long minMoves=curMoves;
+        for(int i=1;i<nums.length;i++){
+            
+            // System.out.println(curMoves+" "+minMoves);
+            curMoves = curMoves + i*Math.abs(nums[i]-nums[i-1]) - Math.abs(nums[i]-nums[i-1])*(nums.length-i);
+            minMoves = Math.min(minMoves,curMoves);
+            // System.out.println(curMoves+" "+minMoves);
         }
-        
-        int randomPivotIndex= (int)(Math.random()*(right-left+1)+left);
-        
-        int partitionIndex = partition(nums,left,right,randomPivotIndex);
-        if(partitionIndex==mid){
-            return nums[partitionIndex];
-        }
-        if(partitionIndex<mid){
-            return quickSelectMedian(nums,partitionIndex+1,right,mid);
-        }else{
-            return quickSelectMedian(nums,left,partitionIndex-1,mid);
-        }
-    }
-    
-    void swap(int[] arr, int left, int right){
-        int t=arr[left];
-        arr[left]=arr[right];
-        arr[right]=t;
-    }
-    
-    int partition(int[] nums, int left, int right, int pIndex){
-        int pivot=nums[pIndex];
-        swap(nums,pIndex,right);
-        
-        int start=left;
-        for(int i=left;i<=right;i++){
-            if(nums[i]<=pivot){
-                swap(nums,start,i);
-                start++;
-            }
-        }
-        return start-1;
+        return (int)minMoves;
     }
 }
