0296053e4c3113c8984f2731fa5dcb3b840079b9
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-08-25 12:28:44 -0500

    Time: 77 ms (29.35%), Space: 49.6 MB (38.29%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        
+        for(int i=n-2;i>=0;i--){
+            dp[i]=n;
+            for(int j=i+1;j<=Math.min(n-1,i+nums[i]);j++){
+                dp[i]=Math.min(dp[i],1+dp[j]);
+            }
+        }
+        return dp[0];
+    }
+}
