8e517cc2139ddd3fc439b4aabaf0c0f60f677e26
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-08 12:09:54 -0400

    Time: 9 ms (63.25%), Space: 49 MB (26.97%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -8,14 +8,13 @@ class Solution {
         }
         
         int[] maxPoints=new int[maxNum+1];
-        maxPoints[1]=count[1];
         for(int i=2;i<=maxNum;i++){
-            maxPoints[i] = Math.max(maxPoints[i-1], maxPoints[i-2] + i* count[i]);
+            count[i] = Math.max(count[i-1], count[i-2] + i* count[i]);
         }
         
         int maxPointEarned=0;
         for(int i=1;i<=maxNum;i++){
-            maxPointEarned=Math.max(maxPointEarned, maxPoints[i]);
+            maxPointEarned=Math.max(maxPointEarned, count[i]);
         }
         
         return maxPointEarned;
