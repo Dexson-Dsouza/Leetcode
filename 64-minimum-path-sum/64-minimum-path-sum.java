8867ae3fff75045e2f36a88f27fb93decb7ab0bb
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-15 17:48:30 -0400

    Time: 2 ms (90.10%), Space: 46.8 MB (7.86%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -3,33 +3,25 @@ class Solution {
         int rows=grid.length;
         int cols=grid[0].length;
         boolean[][] visited=new boolean[rows][cols];
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
-        
-        pq.add(new int[]{grid[0][0],0,0});
-        visited[0][0]=true;
-        
-        while(pq.size()>0){
-            int[] current=pq.poll();
-            
-            int cost=current[0];
-            int row=current[1];
-            int col=current[2];
-            
-            if(row==rows-1 && col==cols-1){
-                return cost;
-            }
-            
-            if(row+1<rows && visited[row+1][col]==false){
-                visited[row+1][col]=true;
-                pq.add(new int[]{cost+grid[row+1][col],row+1,col});
-            }
-            if(col+1<cols && visited[row][col+1]==false){
-                int nr=row;
-                int nc=col+1;
-                visited[nr][nc]=true;
-                pq.add(new int[]{cost+grid[nr][nc],nr,nc});
-            }
+        int[][] minCost=new int[rows][cols];
+        return findCost(grid,0,0,minCost,visited);
+    }
+    
+    int findCost(int[][] grid, int row, int col, int[][] minCost, boolean[][] visited){
+        if(row==grid.length-1 && col==grid[0].length-1){
+            return grid[row][col];
         }
-        return -1;
+        if(visited[row][col]==true){
+            return minCost[row][col];
+        }
+        visited[row][col]=true;
+        int minCostToReach=Integer.MAX_VALUE;
+        if(row+1 < grid.length){
+            minCostToReach=Math.min(minCostToReach, grid[row][col] + findCost(grid,row+1,col,minCost,visited));
+        }
+        if(col+1 < grid[0].length){
+            minCostToReach=Math.min(minCostToReach, grid[row][col] + findCost(grid,row,col+1,minCost,visited));
+        }
+        return minCost[row][col]=minCostToReach;
     }
 }
