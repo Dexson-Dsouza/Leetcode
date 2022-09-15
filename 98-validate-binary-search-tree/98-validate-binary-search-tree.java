d0d64a44cce853aa66afd655b6a83dc7655b7677
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-15 19:20:28 -0400

    Time: 10 ms (5.34%), Space: 44.1 MB (32.80%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,18 +14,42 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return isValid(root,null,null);
+    static class nodeBoundary{
+        TreeNode node;
+        Integer low;
+        Integer high;
+        nodeBoundary(TreeNode node, Integer low, Integer high){
+            this.low=low;
+            this.high=high;
+            this.node=node;
+        }
     }
-     public boolean isValid(TreeNode root, Integer minSoFar, Integer maxSoFar){
-         if(root==null){
-             return true;
-         }
-         
-         if((minSoFar!=null && root.val <= minSoFar) || (maxSoFar !=null && root.val>= maxSoFar)){
-             return false;
-         }
-         return isValid(root.left,minSoFar,root.val) && isValid(root.right,root.val,maxSoFar);
-     }
+    public boolean isValidBST(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        Stack<nodeBoundary> st=new Stack<>();
+        st.push(new nodeBoundary(root,null,null));
+        while(st.size()>0){
+            nodeBoundary currentObject = st.pop();
+            TreeNode currentNode=currentObject.node;
+            Integer low=currentObject.low;
+            Integer high=currentObject.high;
+            if(low!=null && currentNode.val<=low){
+                return false;
+            }
+            if(high!=null && currentNode.val>=high){
+                return false;
+            }
+            
+            if(currentNode.left!=null){
+                st.push(new nodeBoundary(currentNode.left,low,currentNode.val));
+            }
+            if(currentNode.right!=null){
+                st.push(new nodeBoundary(currentNode.right,currentNode.val,high));
+            }
+        }
+        return true;
+    }
     
 }
