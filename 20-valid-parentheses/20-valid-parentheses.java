1b620313b5c4631afb3e6468f330e06eaffe0ceb
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-15 15:00:31 -0400

    Time: 1 ms (99.66%), Space: 40.1 MB (95.86%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack();
+        
+        for(char ch:s.toCharArray()){
+            if(ch=='[' || ch=='(' || ch=='{' ){
+                st.push(ch);
+            }else if(ch==')'){
+                if(st.empty() || st.peek()!='('){
+                    return false;
+                }
+                st.pop();
+            }else if(ch==']'){
+                if(st.empty() || st.peek()!='['){
+                    return false;
+                }
+                st.pop();
+            }else if(ch=='}'){
+                if(st.empty() || st.peek()!='{'){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.size()==0;
+    }
+}
