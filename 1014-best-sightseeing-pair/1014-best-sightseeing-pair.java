4bd224182dc654ec08bd6698fa674a9f1fec3b93
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-20 11:29:36 -0400

    Time: 3 ms (91.54%), Space: 51.7 MB (84.48%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -2,20 +2,14 @@ class Solution {
     public int maxScoreSightseeingPair(int[] values) {
         Deque<int[]> bestPrevSpots=new LinkedList<>();
         
-        bestPrevSpots.add(new int[]{values[0],0});
-        
+        int bestPrevScore = values[0];
         int bestScore=0;
         
         for(int i=1;i<values.length;i++){
-            int[] prevSpot = bestPrevSpots.peekFirst();
-            int curScore = values[i] + prevSpot[0] + (prevSpot[1]-i);
+            int curScore = values[i] + bestPrevScore -i;
             bestScore=Math.max(bestScore,curScore);
             
-            int curSpotScore = i+values[i];
-            while(bestPrevSpots.size()>0 && (bestPrevSpots.peekFirst()[1]+bestPrevSpots.peekFirst()[0])<=curSpotScore){
-                bestPrevSpots.pollFirst();
-            }
-            bestPrevSpots.add(new int[]{values[i],i});
+            bestPrevScore=Math.max(bestPrevScore,values[i]+i);    
         }
         return bestScore;
     }
