83573141639cb362f49e652f7b4c33a6a1532fd2
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-22 10:32:39 -0400

    Time: 16 ms (42.17%), Space: 76.4 MB (22.61%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.java
+++ 1272-remove-interval/1272-remove-interval.java
@@ -1,37 +1,28 @@
 class Solution {
     public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
-        List<List<Integer>> leftIntervals=new ArrayList<>();
-        
-        int removedStart=toBeRemoved[0];
-        int removedEnd=toBeRemoved[1];
+        List<List<Integer>> changedIntervals=new ArrayList<>();
         
         for(int[] interval:intervals){
-            int start=interval[0];
-            int end=interval[1];
+            int from=interval[0];
+            int to=interval[1];
             
-            if(start<removedStart && end >removedEnd){
-                List<Integer> startInterval=new ArrayList<>();
-                startInterval.add(start);
-                startInterval.add(removedStart);
-                leftIntervals.add(startInterval);
-                List<Integer> endInterval=new ArrayList<>();
-                
-                endInterval.add(removedEnd);
-                endInterval.add(end);
-                leftIntervals.add(endInterval);
-            }else if(start<removedStart){
-                List<Integer> startInterval=new ArrayList<>();
-                startInterval.add(start);
-                startInterval.add(Math.min(removedStart,end));
-                leftIntervals.add(startInterval);
-            }else if(end>removedEnd){
-                List<Integer> endInterval=new ArrayList<>();
-                endInterval.add(Math.max(removedEnd,start));
-                endInterval.add(end);
-                leftIntervals.add(endInterval);
+            if(from<toBeRemoved[0]){
+                int start=from;
+                int end=Math.min(toBeRemoved[0],to);
+                List<Integer> removedInterval=new ArrayList<>();
+                removedInterval.add(start);
+                removedInterval.add(end);
+                changedIntervals.add(removedInterval);
             }
-            
-        } 
-        return leftIntervals;
+            if(to>toBeRemoved[1]){
+                int start=Math.max(toBeRemoved[1],from);
+                int end=to;
+                List<Integer> removedInterval=new ArrayList<>();
+                removedInterval.add(start);
+                removedInterval.add(end);
+                changedIntervals.add(removedInterval);
+            }
+        }
+        return changedIntervals;
     }
 }
