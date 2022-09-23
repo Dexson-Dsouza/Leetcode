2f5c218bb9cfced07e3bc9c352ca0a66ca122563
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-23 14:25:28 -0400

    Time: 91 ms (24.02%), Space: 148.7 MB (61.53%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,22 +1,10 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> dq=new LinkedList<>();
-        List<Integer> maxWindowList=new ArrayList<>();
-        for(int i=0;i<k-1;i++){
-            int cur=nums[i];
-            while(dq.size()>0 && i-k>=dq.peekFirst()){
-                dq.pollFirst();
-            }
-            
-            while(dq.size()>0 && nums[dq.peekLast()]<=cur){
-                dq.pollLast();
-            }
-            dq.addLast(i);
-        }
-        
+        int[] maxWindow=new int[nums.length-k+1];
         int index=0;
         
-        for(int i=k-1;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){
             int cur=nums[i];
             while(dq.size()>0 && i-k>=dq.peekFirst()){
                 dq.pollFirst();
@@ -26,13 +14,10 @@ class Solution {
                 dq.pollLast();
             }
             dq.addLast(i);
-            maxWindowList.add(nums[dq.peekFirst()]);
+            if(i>=k-1){
+                maxWindow[index++]=nums[dq.peekFirst()];
+            }
         }
-        int[] result=new int[maxWindowList.size()];
-        for(int i=0;i<maxWindowList.size();i++){
-            result[i]=maxWindowList.get(i);
-        }
-        
-        return result;
+        return maxWindow;
     }
 }
