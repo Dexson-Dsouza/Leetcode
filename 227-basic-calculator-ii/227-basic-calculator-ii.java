1f5a2a7183f28af3289068e547d5fb896cc3a209
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-24 15:36:23 -0400

    Time: 53 ms (13.65%), Space: 46.4 MB (20.55%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Character> operations=new Stack<>();
+        Stack<Integer> numbers=new Stack<>();
+        
+        operations.push('+');
+        
+        int index=0;
+        while(index<s.length()){
+            if(s.charAt(index)==' '){
+                index++;
+            }else if(isNum(s.charAt(index))){
+                
+                int num=0;
+                while(index<s.length() && isNum(s.charAt(index))){
+                    num = num*10 + (s.charAt(index)-'0');
+                    index++;
+                }
+                
+                if(operations.size()>0 &&(operations.peek() =='*' || operations.peek()=='/')){
+                    int firstNum=numbers.pop();
+                    char op = operations.pop();
+                    if(op=='*'){
+                        num=firstNum*num;
+                    }else{
+                        num=firstNum/num;
+                    }
+                }
+                numbers.push(num);
+            }else{
+                operations.push(s.charAt(index++));
+            }
+        }
+        
+        int res=0;
+        while(operations.size()>0){
+            int num=numbers.pop();
+            char op = operations.pop();
+            if(op=='-'){
+                res-=num;
+            }else{
+                res+=num;
+            }
+        }
+        return res;
+    }
+    
+    public boolean isNum(char ch){
+        return ch>='0' && ch<='9';
+    }
+}
