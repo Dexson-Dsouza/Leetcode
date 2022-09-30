bc8e3cea2b9a69154677eaebe1a2b739180f7589
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-09-30 17:01:54 -0400

    Time: 332 ms (15.28%), Space: 43.2 MB (74.44%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.java
+++ 214-shortest-palindrome/214-shortest-palindrome.java
@@ -1,30 +1,13 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        
-        StringBuilder reverseInput= new StringBuilder(s);
-        
-        reverseInput.reverse();
-        String str = s + "$" +reverseInput.toString();
-        
-        int[] kmp=new int[str.length()];
-        
-        for(int i=1;i<str.length();i++){
-            int index=kmp[i-1];
-            
-            while(index>0 && str.charAt(index)!=str.charAt(i)){
-                index = kmp[index-1];
-            }
-            
-            if(str.charAt(index)==str.charAt(i)){
-                index++; 
-            }
-            kmp[i]=index;
-            
-        }
-        int palLength=kmp[str.length()-1];
-        StringBuilder extraString = new StringBuilder(s.substring(palLength));
-        extraString.reverse();
-        
-        return extraString.toString() + s;
+    int i = 0, end = s.length() - 1, j = end; char chs[] = s.toCharArray();
+    while(i < j) {
+         if (chs[i] == chs[j]) {
+             i++; j--;
+         } else { 
+             i = 0; end--; j = end;
+         }
     }
+    return new StringBuilder(s.substring(end+1)).reverse().toString() + s;
+}
 }
