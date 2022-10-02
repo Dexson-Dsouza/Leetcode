ce4ff88ce631d71ee3359e35ec9ed733763de2ef
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-10-01 20:59:24 -0400

    Time: 9 ms (98.35%), Space: 42 MB (96.08%) - LeetHub

---
+++ 1188-design-bounded-blocking-queue/1188-design-bounded-blocking-queue.java
@@ -0,0 +1,27 @@
+class BoundedBlockingQueue {
+    Semaphore  push;
+    Semaphore  pop;
+    Deque<Integer> nums;
+    public BoundedBlockingQueue(int capacity) {
+        nums=new LinkedList<>();
+        push=new Semaphore(capacity);
+        pop=new Semaphore(0);
+    }
+    
+    public void enqueue(int element) throws InterruptedException {
+        push.acquire();
+        nums.add(element);
+        pop.release();
+    }
+    
+    public int dequeue() throws InterruptedException {
+        pop.acquire();
+        int val = nums.poll();
+        push.release();
+        return val;
+    }
+    
+    public int size() {
+        return nums.size();
+    }
+}
