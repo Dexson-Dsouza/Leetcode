773550423c858fca9c0746c67ceb9265a341591a
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-10-06 10:51:58 -0400

    Time: 231 ms (36.55%), Space: 117.4 MB (18.14%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,39 +1,37 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> set = new HashSet<>();
-        for(String word:wordList){
-            set.add(word);
-        }
-        if(!set.contains(endWord))
-            return 0;
-        Queue<String> queue=new LinkedList<>();
+        Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(endWord)) return 0;
+        
+        Queue<String> queue = new LinkedList<>();
         queue.add(beginWord);
-        int level=1;
+        
+        Set<String> visited = new HashSet<>();
+        queue.add(beginWord);
+        
+        int changes = 1;
         
         while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                String currWord=queue.poll();
-                char[]wordChar=currWord.toCharArray();
-                for(int j=0;j<wordChar.length;j++){
-                    char oldChar=wordChar[j];
-                    for(char c='a';c<='z';c++){
-                        if(oldChar==c) continue;
-                        wordChar[j]=c;
-                        String newWord=String.valueOf(wordChar);
-                        if(newWord.equals(endWord))
-                            return level+1;
-                        if(set.contains(newWord)){
-                            queue.offer(newWord);
-                            set.remove(newWord);
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                String word = queue.poll();
+                if(word.equals(endWord)) return changes;
+                
+                for(int j = 0; j < word.length(); j++){
+                    for(int k = 'a'; k <= 'z'; k++){
+                        char arr[] = word.toCharArray();
+                        arr[j] = (char) k;
+                        
+                        String str = new String(arr);
+                        if(set.contains(str) && !visited.contains(str)){
+                            queue.add(str);
+                            visited.add(str);
                         }
                     }
-                    wordChar[j]=oldChar;
                 }
             }
-            level++;
+            ++changes;
         }
-        
         return 0;
     }
 }
