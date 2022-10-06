ad08e8a841947a524c890f97ae937eced1d99c40
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-10-06 11:21:30 -0400

    Time: 14 ms (81.01%), Space: 44.5 MB (58.50%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.java
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.java
@@ -1,59 +1,83 @@
 class Leaderboard {
-    Map<Integer,Integer> playerScoreMap;
-    TreeMap<Integer,Integer> scoreCountMap;
+
+    Map<Integer, Integer> scores;
+    TreeMap<Integer, Integer> sortedScores;
+    
     public Leaderboard() {
-        playerScoreMap=new HashMap<>();
-        scoreCountMap=new TreeMap<>();
+        this.scores = new HashMap<Integer, Integer>();
+        this.sortedScores = new TreeMap<>(Collections.reverseOrder());
     }
     
     public void addScore(int playerId, int score) {
-        if(playerScoreMap.containsKey(playerId)){
-            int currentScore = playerScoreMap.get(playerId);
-            update(currentScore);
+        
+        // The scores dictionary simply contains the mapping from the
+        // playerId to their score. The sortedScores contain a BST with 
+        // key as the score and value as the number of players that have
+        // that score.        
+        if (!this.scores.containsKey(playerId)) {
+            this.scores.put(playerId, score);
+            this.sortedScores.put(score, this.sortedScores.getOrDefault(score, 0) + 1);
+        } else {
+            
+            // Since the current player's score is changing, we need to
+            // update the sortedScores map to reduce count for the old
+            // score.
+            int preScore = this.scores.get(playerId);
+            int playerCount = this.sortedScores.get(preScore);
+            
+            
+            // If no player has this score, remov it from the tree.
+            if (playerCount == 1) {
+                this.sortedScores.remove(preScore);
+            } else {
+                this.sortedScores.put(preScore, playerCount - 1);
+            }
+            
+            // Updated score
+            int newScore = preScore + score;
+            this.scores.put(playerId, newScore);
+            this.sortedScores.put(newScore, this.sortedScores.getOrDefault(newScore, 0) + 1);
         }
-        playerScoreMap.put(playerId,playerScoreMap.getOrDefault(playerId,0)+score);
-        int latestScore=playerScoreMap.get(playerId);
-        scoreCountMap.put(latestScore,scoreCountMap.getOrDefault(latestScore,0)+1);
-    }
-    
-    
-    public void update(int score){
-        scoreCountMap.put(score,scoreCountMap.get(score)-1);
-        if(scoreCountMap.get(score)==0){
-            scoreCountMap.remove(score);
-        }    
     }
     
     public int top(int K) {
         
-        // System.out.println(playerScoreMap);
-        // System.out.println(scoreCountMap);
-        List<Integer> scores = new ArrayList<>(scoreCountMap.keySet());
-        int sum=0;
-        for(int i=scores.size()-1;i>=0;i--){
-            int score = scores.get(i);
-            int count=scoreCountMap.get(score);
-            int required = Math.min(K,count);
-            K-=required;
-            sum = sum + score*required;
-            if(K==0){
+        int count = 0;
+        int sum = 0;
+        
+        // In-order traversal over the scores in the TreeMap
+        for (Map.Entry<Integer, Integer> entry: this.sortedScores.entrySet()) {
+            
+            // Number of players that have this score.
+            int times = entry.getValue();
+            int key = entry.getKey();
+            
+            for (int i = 0; i < times; i++) {
+                sum += key;
+                count++;
+                
+                // Found top-K scores, break.
+                if (count == K) {
+                    break;
+                }
+            }
+            
+            // Found top-K scores, break.
+            if (count == K) {
                 break;
             }
         }
+        
         return sum;
     }
     
     public void reset(int playerId) {
-        int score = playerScoreMap.get(playerId);
-        update(score);
-        playerScoreMap.remove(playerId);
+        int preScore = this.scores.get(playerId);
+        this.sortedScores.put(preScore, this.sortedScores.get(preScore) - 1);
+        if (this.sortedScores.get(preScore) == 0) {
+            this.sortedScores.remove(preScore);
+        }
+        
+        this.scores.remove(playerId);
     }
 }
-
-/**
- * Your Leaderboard object will be instantiated and called as such:
- * Leaderboard obj = new Leaderboard();
- * obj.addScore(playerId,score);
- * int param_2 = obj.top(K);
- * obj.reset(playerId);
- */
