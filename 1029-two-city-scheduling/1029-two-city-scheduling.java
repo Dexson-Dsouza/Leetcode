cb0879271c83a685573c892761064730c9861bc8
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-10-10 13:34:17 -0400

    Time: 2 ms (90.27%), Space: 42.8 MB (10.02%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,16 +1,20 @@
 class Solution {
-    public int twoCitySchedCost(int[][] costs) {
-        int totalPeople=costs.length;
-        
-        Arrays.sort(costs,(a,b)->(a[0]-a[1])-(b[0]-b[1]));
-        
-        int totalCost=0;
-        for(int i=0;i<totalPeople/2;i++){
-            totalCost+=costs[i][0];
-        }
-        for(int i=totalPeople/2;i<totalPeople;i++){
-            totalCost+=costs[i][1];
-        }
-        return totalCost;
-    }
+      public int twoCitySchedCost(int[][] costs) {
+        int n = costs.length;
+        int[][] memo = new int[n][n];
+        return helper(costs, 0, 0, n, memo);
+      }
+
+      private int helper(int[][] costs, int a, int b, int n, int[][] memo) {
+        if (a + b == n) return 0;
+        if (memo[a][b] != 0) return memo[a][b];
+
+        int take = Integer.MAX_VALUE;
+        int skip = Integer.MAX_VALUE;
+        if (a < n / 2) take = costs[a + b][0] + helper(costs, a + 1, b, n, memo);
+        if (b < n / 2) skip = costs[a + b][1] + helper(costs, a, b + 1, n, memo);
+        memo[a][b] = Math.min(take, skip);
+
+        return memo[a][b];
+      }
 }
