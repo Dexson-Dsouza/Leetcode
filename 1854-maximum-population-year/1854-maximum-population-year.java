b156ef0acf5b0721e446b3f733e603309bad9d72
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-10-11 14:07:36 -0400

    Time: 4 ms (23.61%), Space: 42.2 MB (53.81%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,25 +1,25 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        int[] count=new int[1001];
+        int[] count=new int[2051];
         
         int maxCount=0;
         int maxYear=0;
         for(int log[]:logs){
-            int s=log[0]-1950;
-            int e=log[1]-1950;
+            int s=log[0];
+            int e=log[1];
             count[s]++;
             count[e]--;
         }
         
         int curCount=0;
         
-        for(int i=0;i<=1000;i++){
+        for(int i=0;i<=2050;i++){
             curCount+=count[i];
             if(curCount>maxCount){
                 maxCount=curCount;
                 maxYear=i;
             }
         }
-        return maxYear+1950;
+        return maxYear;
     }
 }
