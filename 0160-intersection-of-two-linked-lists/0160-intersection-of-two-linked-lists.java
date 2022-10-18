3a75d055659ee19074568a7562e27285cbfd97df
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-10-18 18:46:02 -0400

    Time: 6 ms (44.61%), Space: 55.4 MB (40.05%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,36 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode ptr1=headA;
-        ListNode ptr2=headB;
-        boolean isPtr1Turned=false;
-        boolean isPtr2Turned=false;
-        
-        while(ptr1!=null && ptr2!=null){
-            if(ptr1==ptr2){
-                return ptr1;
-            }
-            ptr1=ptr1.next;
-            ptr2=ptr2.next;
-            if(ptr1==null && isPtr1Turned==false){
-                isPtr1Turned=true;
-                ptr1=headB;
-            }
-            if(ptr2==null && isPtr2Turned==false){
-                isPtr2Turned=true;
-                ptr2=headA;
-            }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
         }
-        return null;
+        return pA;
+        // Note: In the case lists do not intersect, the pointers for A and B
+        // will still line up in the 2nd iteration, just that here won't be
+        // a common node down the list and both will reach their respective ends
+        // at the same time. So pA will be NULL in that case.
     }
 }
