4cc0fcc05e78aa92ce4a86e2d0957d5261287b04
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-10-30 12:50:19 -0400

    Time: 26 ms (75.59%), Space: 42.9 MB (87.88%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,43 +1,57 @@
-class Solution {
-    public int shortestPath(int[][] grid, int k) {
-        int rows=grid.length;
-        int cols=grid[0].length;
-        Set<String> visitedStates=new HashSet<>();
-        Queue<int[]> currentStates=new LinkedList<>();
-        currentStates.add(new int[]{0,0,k,0});
-        String startKey = 0+"*"+0+"*"+k;
-        visitedStates.add(startKey);
-        int[] dirs=new int[]{0,-1,0,1,0};
-        int minimumMoves=Integer.MAX_VALUE;
-        boolean[][][] movesToReach=new boolean[rows][cols][k+1];
-        movesToReach[0][0][0]=true;
-        while(currentStates.size()>0){
-            int[] currentState=currentStates.poll();
-            
-            int row=currentState[0];
-            int col=currentState[1];
-            int kCount=currentState[2];
-            int moves=currentState[3];
-            if(row==rows-1 && col==cols-1){
-                minimumMoves=Math.min(minimumMoves,moves);
-                continue;
-            }
-            for(int i=0;i<4;i++){
-                int nextRow=row+dirs[i];
-                int nextCol=col+dirs[i+1];
-                if(nextRow>=0 && nextRow<rows && nextCol>=0 && nextCol<cols && kCount>=grid[nextRow][nextCol]){
-                    int remCount=kCount-grid[nextRow][nextCol];
-                    String stateKey = nextRow+"*"+nextCol+"*"+remCount;
-                    if(movesToReach[nextRow][nextCol][remCount]==false){
-                        movesToReach[nextRow][nextCol][remCount]=true;
-
-                        // System.out.println(stateKey);
-                        currentStates.add(new int[]{nextRow,nextCol,remCount,moves+1});
-                    }
-                }
-
-            }
-        }
-        return minimumMoves==Integer.MAX_VALUE?-1:minimumMoves;
-    }
+class gridPoint{
+	int r;
+	int c; 
+	int k;
+	gridPoint(int r, int c, int k){
+	this.r=r;
+	this.c=c;
+	this.k=k;
 }
+}
+
+class Solution {
+	int maxSteps=Integer.MAX_VALUE;
+
+public int shortestPath(int[][] grid, int k) {
+        	int rows=grid.length;
+        	int cols=grid[0].length;
+int steps=0;
+Queue<gridPoint> gridPoints=new LinkedList<>();
+	gridPoints.add(new gridPoint(0,0,k));
+    boolean[][][] visited = new boolean[rows][cols][k+1];
+	visited[0][0][k]=true;
+	while(gridPoints.size()>0){
+		int curSize=gridPoints.size();
+		for(int i=0;i<curSize;i++){
+			gridPoint curPoint=gridPoints.poll();
+            // System.out.println(curPoint.r + " "+curPoint.c + " "+curPoint.k);
+			if(curPoint.r==rows-1 && curPoint.c==cols-1){
+return steps;	
+}
+for(int dir=0;dir<4;dir++){
+	int nextRow=curPoint.r+dirs[dir];
+	int nextCol=curPoint.c+dirs[dir+1];
+	if(isValid(nextRow,nextCol,grid) && curPoint.k>=grid[nextRow][nextCol] && !visited[nextRow][nextCol][curPoint.k-grid[nextRow][nextCol]]){
+visited[nextRow][nextCol][curPoint.k-grid[nextRow][nextCol]]=true;
+gridPoints.add(new gridPoint(nextRow,nextCol,curPoint.k-grid[nextRow][nextCol]));
+
+}
+}
+}	
+steps++;
+
+}
+return -1;
+    	}
+
+	int[] dirs = {0,-1,0,1,0};
+
+
+boolean isValid(int r, int c, int[][] grid){
+	int rows=grid.length;
+	int cols=grid[0].length;
+	return r>=0 && r<rows && c>=0 && c<cols;
+}
+}
+
+
