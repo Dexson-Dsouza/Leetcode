ffd86f599b3389d1176545ec2c4db8caf56dfe45
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-10-31 21:31:24 -0400

    Time: 11 ms (39.18%), Space: 69.9 MB (77.96%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.java
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.java
@@ -1,15 +1,36 @@
 class Solution {
     public int smallestCommonElement(int[][] mat) {
-        Map<Integer,Integer> count=new HashMap<>();
         int rows=mat.length;
         int cols=mat[0].length;
-        for(int col = 0; col < cols; col++){
+        
+        int[] indexes=new int[rows];
+        boolean canFind=true;
+        while(canFind){
+            int largest=-1;
+            for(int row=0;row<rows;row++){
+                int col=indexes[row];
+                int curVal = mat[row][col];
+                largest = Math.max(largest,curVal);
+            }
+            int count = 0;
             for(int row = 0; row < rows; row++){
-                int curVal=mat[row][col];
-                count.put(curVal,count.getOrDefault(curVal,0)+1);
-                if(count.get(curVal)==rows){
-                    return curVal;
+                int col=indexes[row];
+                int curVal = mat[row][col];
+                if(curVal==largest){
+                    count++;
+                }else{
+                    while(col<cols && mat[row][col]<largest){
+                        col++;
+                    }
+                    if(col==cols){
+                        canFind=false;
+                    }
+                    indexes[row]=col;
                 }
+            }
+            
+            if(count==rows){
+                return largest;
             }
         }
         return -1;
