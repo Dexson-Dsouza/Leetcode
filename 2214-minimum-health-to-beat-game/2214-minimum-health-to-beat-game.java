a8bb43757e68c52a76992fb9249dcd0de5e07140
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-11-28 14:54:32 -0500

    Time: 13 ms (28.12%), Space: 77.5 MB (55.09%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.java
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.java
@@ -1,14 +1,13 @@
 class Solution {
     public long minimumHealth(int[] damage, int armor) {
+        int maxDamage = 0;
         long damageSum= 0;
         for(int d:damage){
             damageSum+=d;
+            maxDamage = Math.max(maxDamage,d);
         }
-        long healthRequired = Long.MAX_VALUE;
-        for(int d:damage){
-            int damageTakeWithArmon = Math.max(0,d-armor);
-            healthRequired=Math.min(healthRequired, damageSum - d +damageTakeWithArmon);
-        }
+        int damageWithArmor = Math.max(0 , maxDamage - armor);
+        long healthRequired = damageWithArmor + damageSum - maxDamage;
         
         return healthRequired+1;
     }
