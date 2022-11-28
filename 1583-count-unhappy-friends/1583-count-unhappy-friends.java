0ff433b57b818e9c463d565171d66ef8bfa82bed
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-11-28 18:37:25 -0500

    Time: 3 ms (94.78%), Space: 62.2 MB (96.45%) - LeetHub

--- 1583-count-unhappy-friends/1583-count-unhappy-friends.java
+++ 1583-count-unhappy-friends/1583-count-unhappy-friends.java
@@ -1,44 +1,53 @@
 class Solution {
     public int unhappyFriends(int n, int[][] preferences, int[][] pairs) {
-        int[][] preferenceRank=new int[n][n];
+        int[][] rank = new int[n][n];
+        Map<Integer,Integer> pairMap = new HashMap<>();
         
-        for(int i=0;i<n;i++){
-            int curPerson=i;
-            for(int j=0;j<n-1;j++){
-                int preferredPerson=preferences[i][j];
-                preferenceRank[curPerson][preferredPerson]=j;
+        for(int[] pair:pairs){
+            pairMap.put(pair[0],pair[1]);
+            pairMap.put(pair[1],pair[0]);
+        }
+        
+        for(int i=0;i<preferences.length;i++){
+            int x = i;
+            for(int j=0;j<preferences[i].length;j++){
+                int y = preferences[i][j];
+                int preferenceRank = j;
+                rank[i][y] = preferenceRank;
             }
         }
         
-        int unhappyCount=0;
-        int[] pairArr=new int[n];
-        for(int pair[]:pairs){
-            int x=pair[0];
-            int y=pair[1];
-            pairArr[x]=y;
-            pairArr[y]=x;
-        }
+        int countOfUnhappyFriends=0;
         
-        
-        for(int i=0;i<n;i++){
-            int currentPerson = i;
-            int pairedPerson = pairArr[i];
-            
-            int j=0;
-            while(preferences[currentPerson][j]!=pairedPerson){
-                int preferredPerson = preferences[currentPerson][j];
-                int preferredPersonPair = pairArr[preferredPerson];
-                
-                int preferenceForCurrent = preferenceRank[preferredPerson][currentPerson];
-                int preferredPersonPairPreference = preferenceRank[preferredPerson][preferredPersonPair];
-                if(preferenceForCurrent<preferredPersonPairPreference){
-                    unhappyCount++;
-                    break;
-                }
-                j++;
+        for(int[] pair:pairs){
+            int friend1 = pair[0];
+            int friend2 = pair[1];
+            if(isUnhappy(friend1,friend2,preferences,rank,pairMap)){
+                countOfUnhappyFriends++;
             }
-            
+            if(isUnhappy(friend2,friend1,preferences,rank,pairMap)){
+                countOfUnhappyFriends++;
+            }
         }
-        return unhappyCount;
+        
+        return countOfUnhappyFriends;
+    }
+    
+    boolean isUnhappy(int x, int y, int[][] preferences, int[][] rank, Map<Integer,Integer> pairMap){
+        int rankY = rank[x][y];
+        
+        for(int i=0;i<rankY;i++){
+            int person = preferences[x][i];
+            int personPair = pairMap.get(person);
+            
+            int preferenceX = rank[person][x];
+            int preferencePair = rank[person][personPair];
+            if(preferenceX<preferencePair){
+                // System.out.println(x);
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
