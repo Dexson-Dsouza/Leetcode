b81cd81b326a31759889ff0d12242b8a8045f165
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-11-28 23:26:04 -0500

    Time: 2 ms (90.59%), Space: 43 MB (7.37%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -2,17 +2,17 @@ class Solution {
       public int twoCitySchedCost(int[][] costs) {
         int n = costs.length;
         int[][] memo = new int[n][n];
-        return helper(costs, 0, 0, n, memo);
+        return helper(costs, n/2, n/2, n, memo);
       }
 
       private int helper(int[][] costs, int a, int b, int n, int[][] memo) {
-        if (a + b == n) return 0;
+        if (a + b == 0) return 0;
         if (memo[a][b] != 0) return memo[a][b];
 
         int take = Integer.MAX_VALUE;
         int skip = Integer.MAX_VALUE;
-        if (a < n / 2) take = costs[a + b][0] + helper(costs, a + 1, b, n, memo);
-        if (b < n / 2) skip = costs[a + b][1] + helper(costs, a, b + 1, n, memo);
+        if (a > 0) take = costs[a + b - 1][0] + helper(costs, a - 1, b, n, memo);
+        if (b > 0) skip = costs[a + b - 1][1] + helper(costs, a, b - 1, n, memo);
         memo[a][b] = Math.min(take, skip);
 
         return memo[a][b];
