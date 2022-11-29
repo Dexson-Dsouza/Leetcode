5ea744e6bfdccccc0144603cb99cbf90503b1724
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-11-29 18:23:15 -0500

    Time: 1 ms (90.80%), Space: 41.8 MB (44.83%) - LeetHub

--- 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.java
+++ 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.java
@@ -8,50 +8,25 @@
 
 class Solution {
     public int countShips(Sea sea, int[] topRight, int[] bottomLeft) {
-        return divideAndFind(sea,topRight,bottomLeft);
-    }
-    
-    public int divideAndFind(Sea sea, int[] topRight, int[] bottomLeft){
-        // System.out.println(bottomLeft[0]+" "+bottomLeft[1] +" "+ topRight[0]+" "+topRight[1] +" "+sea.hasShips(topRight,bottomLeft));
-        
-        
-        if(topRight[0]<bottomLeft[0] || topRight[1]<bottomLeft[1]){
-            return 0;
-        }
+        int x1 = bottomLeft[0];
+        int y1 = bottomLeft[1];
+        int x2 = topRight[0];
+        int y2 = topRight[1];
         
         if(sea.hasShips(topRight,bottomLeft)==false){
             return 0;
         }
-        
-        
-        if(bottomLeft[0]==topRight[0] && bottomLeft[1]==topRight[1]){
-            return sea.hasShips(topRight,bottomLeft)==true?1:0;
+        if(x1==x2 && y1==y2){
+            return 1;
         }
         
-        int xMid = (topRight[0] + bottomLeft[0])/2;
-        int yMid = (topRight[1] + bottomLeft[1])/2;
+        int midX = (x1+x2)/2;
+        int midY = (y1+y2)/2;
         
-//         if(xMid==0 && yMid==0){
-            
-//         }
-        
-        int[] top1=new int[]{xMid,yMid};
-        int[] bot1=bottomLeft.clone();
-        int count1 = divideAndFind(sea,top1,bot1);
-        
-        int[] top2=new int[]{topRight[0],yMid};
-        int[] bot2=new int[]{xMid+1,bottomLeft[1]};
-        int count2 = divideAndFind(sea,top2,bot2);
-        
-        int[] top3=new int[]{xMid,topRight[1]};
-        int[] bot3=new int[]{bottomLeft[0],yMid+1};
-        int count3 = divideAndFind(sea,top3,bot3);
-        
-        int[] top4=topRight.clone();
-        int[] bot4=new int[]{xMid+1,yMid+1};
-        int count4 = divideAndFind(sea,top4,bot4);
-        
-        
-        return count1+count2+count3+count4;
+        int leftBottom = countShips(sea,new int[]{midX,midY},new int[]{x1,y1});
+        int rightBottom = countShips(sea,new int[]{x2,midY},new int[]{midX+1,y1});
+        int leftTop = countShips(sea,new int[]{midX,y2},new int[]{x1,midY+1});
+        int rightTop = countShips(sea,new int[]{x2,y2},new int[]{midX+1,midY+1});
+        return leftBottom+rightBottom+leftTop+rightTop;
     }
 }
