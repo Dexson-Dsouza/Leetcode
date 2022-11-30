14a2f91e3a536cd35af5b8eb175a60282ac0b97e
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-11-30 17:28:59 -0500

    Time: 100 ms (63.41%), Space: 67 MB (32.08%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -5,16 +5,55 @@ class Solution {
         for(int i=lo;i<=hi;i++){
             findPower(i,powerMap);
         }
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]==b[1]?b[0]-a[0]:b[1]-a[1]);
         
+        int[][] nums=new int[hi-lo+1][2];
         for(int i=lo;i<=hi;i++){
-            // System.out.println(i+" "+findPower(i,powerMap));
-            pq.add(new int[]{i,findPower(i,powerMap)});
-            if(pq.size()>k){
-                pq.poll();
-            }
+            int index=i-lo;
+            nums[index][0] = i;
+            nums[index][1] = findPower(i,powerMap);
         }
-        return pq.peek()[0];
+        return quickSelect(nums,0,nums.length-1,k-1);
+    }
+    
+    int quickSelect(int[][] nums, int left, int right, int k){
+        int pivot = findPivot(nums,left,right);
+        if(pivot==k){
+            return nums[pivot][0];
+        }
+        
+        if(pivot<k){
+            return quickSelect(nums,pivot+1,right,k);
+        }else{
+            return quickSelect(nums,left,pivot-1,k);
+        }
+    }
+    
+    int findPivot(int[][] nums, int left, int right){
+        // if(left>=right){
+        //     return nums[left][0];
+        // }
+        int pivotPower = nums[right][1];
+        int pivotNum = nums[right][0];
+        int k = left;
+        int i = left;
+        while(i<right){
+            int pow = nums[i][1];
+            int num = nums[i][0];
+            if(pow < pivotPower ||(pow == pivotPower && num<pivotNum)){
+                int[] t = nums[k];
+                nums[k] = nums[i];
+                nums[i] = t;
+                k++;
+            }
+            i++;
+        }
+        int pow = nums[k][1];
+        int n = nums[k][0];
+        nums[k][0] = nums[right][0];
+        nums[k][1] = nums[right][1];
+        nums[right][0] = n; 
+        nums[right][1] = pow; 
+        return k;
     }
     
     public int findPower(int n, Map<Integer,Integer> powerMap){
