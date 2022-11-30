8c6f0cea8c82cf19dc6a49a1c3d1199b2f939188
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-11-30 16:22:52 -0500

    Time: 77 ms (87.56%), Space: 109.9 MB (56.19%) - LeetHub

--- 0716-max-stack/0716-max-stack.java
+++ 0716-max-stack/0716-max-stack.java
@@ -2,50 +2,53 @@ class MaxStack {
     private Stack<int[]> stack;
     private PriorityQueue<int[]> heap;
     private Set<Integer> removed;
-    private int cnt; // ID of each number
+    private int id; // ID of each number
 
     public MaxStack() {
-        cnt=0;
+        id=0;
         stack = new Stack<>();
         heap = new PriorityQueue<>((a, b) -> b[0] - a[0] == 0 ? b[1] - a[1] : b[0] - a[0]);
         removed = new HashSet<>();
     }
 
     public void push(int x) {
-        stack.add(new int[] { x, cnt });
-        heap.add(new int[] { x, cnt });
-        cnt++;
+        stack.add(new int[] { x, id });
+        heap.add(new int[] { x, id });
+        id++;
     }
 
     public int pop() {
-        removePoppedElements();
+        removeStackPoppedElements();
         int[] top = stack.pop();
         removed.add(top[1]);
         return top[0];
     }
 
     public int top() {
-        removePoppedElements();
+        removeStackPoppedElements();
         return stack.peek()[0];
     }
 
     public int peekMax() {
-        while (removed.contains(heap.peek()[1])) {
-            heap.poll();
-        }
+        
+        removeMaxPoppedElements();
         return heap.peek()[0];
     }
     
-    void removePoppedElements(){
+    void removeMaxPoppedElements(){
+         while (removed.contains(heap.peek()[1])) {
+            heap.poll();
+        }
+    }
+    
+    void removeStackPoppedElements(){
          while (removed.contains(stack.peek()[1])) {
             stack.pop();
         }
     }
 
     public int popMax() {
-        while (removed.contains(heap.peek()[1])) {
-            heap.poll();
-        }
+        removeMaxPoppedElements();
         int[] top = heap.poll();
         removed.add(top[1]);
         return top[0];
