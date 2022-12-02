bcc04d6ff15b7f7e1b67f6c963da8e7a1bce5ab3
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-12-01 20:34:35 -0500

    Time: 734 ms (10.09%), Space: 117.6 MB (7.44%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -5,18 +5,19 @@ class Solution {
         return findMinCost(0,n,cuts,0,cuts.length-1,minCostMap);
     }
     
-    int findMinCost(int start, int end, int[] cuts, int l, int r, Map<Integer,Map<Integer,Integer>> minCostMap){
-        int len = end-start;
+    int findMinCost(int startPoint, int endPoint, int[] cuts, int lIndex, int rIndex,
+                    Map<Integer,Map<Integer,Integer>> minCostMap){
+        int len = endPoint-startPoint;
         
-        if(l>r){
+        if(lIndex>rIndex){
             return 0;
         }
-        if(l==r){
+        if(lIndex==rIndex){
             return len;
         }
         
-        if(minCostMap.containsKey(len) && minCostMap.get(len).containsKey(l)){
-            return minCostMap.get(len).get(l);
+        if(minCostMap.containsKey(len) && minCostMap.get(len).containsKey(lIndex)){
+            return minCostMap.get(len).get(lIndex);
         }
         if(minCostMap.containsKey(len)==false){
             minCostMap.put(len,new HashMap<>());
@@ -24,17 +25,17 @@ class Solution {
         
         int minCostToCut = Integer.MAX_VALUE;
         
-        for(int index = l; index<=r; index++){
+        for(int index = lIndex; index<=rIndex; index++){
             
-            int leftLen = cuts[index] - start;
+            int leftLen = cuts[index] - startPoint;
             
             minCostToCut = Math.min(minCostToCut, 
                                     len + 
-                                    findMinCost(start,cuts[index],cuts,l,index-1,minCostMap) +
-                                    findMinCost(cuts[index],end,cuts,index+1,r,minCostMap));
+                                    findMinCost(startPoint,cuts[index],cuts,lIndex,index-1,minCostMap) +
+                                    findMinCost(cuts[index],endPoint,cuts,index+1,rIndex,minCostMap));
         }
         
-        minCostMap.get(len).put(l,minCostToCut);
+        minCostMap.get(len).put(lIndex,minCostToCut);
         return minCostToCut;
     }
 }
