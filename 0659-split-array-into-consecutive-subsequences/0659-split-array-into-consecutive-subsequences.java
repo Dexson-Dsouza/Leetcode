18cd053b9e840a94f2589b85035e5f8b633751c8
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-12-03 18:06:41 -0500

    Time: 45 ms (58.70%), Space: 71.9 MB (45.37%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.java
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.java
@@ -1,26 +1,31 @@
 class Solution {
     public boolean isPossible(int[] nums) {
-        Map<Integer,PriorityQueue<Integer>> sequenceLengthMap=new HashMap<>();
-        
+        Map<Integer,Integer> count=new HashMap<>();
         for(int n:nums){
-            // System.out.println(n + " " + sequenceLengthMap.keySet());
-            int nextLen = 1;
-            if(sequenceLengthMap.containsKey(n) ){
-                int prevLen = sequenceLengthMap.get(n).poll();
-                if(sequenceLengthMap.get(n).size()==0){
-                    sequenceLengthMap.remove(n);
-                }
-                nextLen = prevLen + 1;
-            }
-            int nextNum = n+1;
-            if(sequenceLengthMap.containsKey(nextNum)==false){
-                sequenceLengthMap.put(nextNum,new PriorityQueue<>());
-            }
-            sequenceLengthMap.get(nextNum).add(nextLen);
+            count.put(n,count.getOrDefault(n,0)+1);
         }
         
-        for(int key : sequenceLengthMap.keySet()){
-            if(sequenceLengthMap.get(key).peek()<3){
+        Map<Integer,Integer> exisitingSeqs=new HashMap<>();
+        
+        for(int n:nums){
+            if(count.get(n)==0){
+                continue;
+            }
+            if(exisitingSeqs.getOrDefault(n,0)>0){
+                exisitingSeqs.put(n,exisitingSeqs.get(n)-1);
+                exisitingSeqs.put(n+1,exisitingSeqs.getOrDefault(n+1,0)+1);
+            }else if(count.getOrDefault(n+1,0)>0 && count.getOrDefault(n+2,0)>0){
+                count.put(n+1,count.get(n+1)-1);
+                count.put(n+2,count.get(n+2)-1);
+                exisitingSeqs.put(n+3,exisitingSeqs.getOrDefault(n+3,0)+1);
+            }else{
+                return false;
+            }
+            count.put(n,count.get(n)-1);
+        }
+        
+        for(int n:count.keySet()){
+            if(count.get(n)!=0){
                 return false;
             }
         }
