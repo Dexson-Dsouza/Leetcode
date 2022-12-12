b86db138c1fc110f8638963ff0fd5034f9f84e33
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2022-12-12 10:52:26 -0500

    Time: 80 ms (70.37%), Space: 42.4 MB (7.90%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const steps = [];
+    steps[0]=1;
+    steps[1]=2;
+    for(let i=2;i<n;i++){
+        steps[i] = steps[i-1]+steps[i-2];
+    }
+    return steps[n-1];
+};
