7ce64627e44571c29b7ae199a2475ed6287b0472
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-01-10 13:58:21 -0500

    Time: 0 ms (100.00%), Space: 39.9 MB (78.77%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.java
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -1,40 +1,28 @@
 class Solution {
     public boolean circularArrayLoop(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            int slow, fast;
-            slow = fast = i;
-            boolean isForward = nums[i] > 0;
-            
-            while(true){
-                slow = getNextIndex(nums, slow, isForward);
-                if(slow == -1)
-                    break;
-                
-                fast = getNextIndex(nums, fast, isForward);
-                if(fast == -1)
-                    break;
-                
-                fast = getNextIndex(nums, fast, isForward);
-                if(fast == -1)
-                    break;
-                
-                if(slow == fast)
-                    return true;
-            }
+        int[] color = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(color[i] == 0 && DFS(nums, color, i)) return true;
         }
         return false;
     }
     
-    public int getNextIndex(int[] nums, int index, boolean curDirection){
-        
-        boolean direction = nums[index] >= 0;
-        if(direction != curDirection) return -1; // Shold be either forward/backward direction.
-        
-        int nextIndex = (index + nums[index]) % nums.length;
-        if(nextIndex < 0) nextIndex += nums.length;
-        
-        if(nextIndex == index) return -1; // to ignore case like [-1], [3,1,1] that have 1 element loop
-        
-        return nextIndex;
+    private boolean DFS(int[] nums, int[] color, int start) {
+        //return true if find cycle
+        if(color[start] == 2) return false;
+        color[start] = 1;
+        int next = start + nums[start];
+        next = next % nums.length + nums.length;
+        next %= nums.length;
+        if(next == start || nums[next] * nums[start] < 0) {
+            color[start] = 2;
+            return false;
+        }
+        if(color[next] == 1) {
+            return true;
+        }
+        if(DFS(nums, color, next)) return true;
+        color[start] = 2;
+        return false;
     }
 }
