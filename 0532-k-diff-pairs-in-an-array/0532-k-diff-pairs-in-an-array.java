d0842e93557b253eba646b7e8a21046b16062504
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-01-12 12:49:29 -0500

    Time: 28 ms (33.64%), Space: 44.6 MB (58.07%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,17 +1,20 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
-        Arrays.sort(nums);
         Set<Integer> numSet=new HashSet<>();
-        Set<Integer> doneSet=new HashSet<>();
+        Set<String> doneSet=new HashSet<>();
         int pairCount = 0;
         for(int n:nums){
             int prevPair = n - k;
-            if(doneSet.contains(n)){
-                continue;
-            }
-            if(numSet.contains(prevPair)){
+            String key1 = prevPair + "*"+n;
+            if(doneSet.contains(key1)==false && numSet.contains(prevPair)){
                 pairCount++;
-                doneSet.add(n);
+                doneSet.add(key1);
+            }
+            int nextPair = n + k;
+            String key2= n+"*"+nextPair;
+            if(doneSet.contains(key2)==false && numSet.contains(nextPair)){
+                pairCount++;
+                doneSet.add(key2);
             }
             numSet.add(n);
         }
