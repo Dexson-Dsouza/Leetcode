b0935cea49b9da90edd70d572b3f9cbf6d4cdcb6
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-01-20 12:53:02 -0500

    Time: 6 ms (87.64%), Space: 47.8 MB (98.55%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> perm = new ArrayList<>();
+        generateSubseq(0,nums,perm,result);
+        return result;
+    }
+    
+    public void generateSubseq(int index, int[] nums, List<Integer> perm, List<List<Integer>> result){
+        if(perm.size()>=2){
+            result.add(new ArrayList<>(perm));
+        }
+        Set<Integer> doneSet = new HashSet<>();
+        for(int i = index; i<nums.length; i++){
+            if(doneSet.contains(nums[i])){
+                continue;
+            }
+            if(perm.size()==0 || perm.get(perm.size()-1)<=nums[i]){
+                doneSet.add(nums[i]);
+                perm.add(nums[i]);
+                generateSubseq(i+1,nums,perm,result);
+                perm.remove(perm.size()-1);
+            }
+        }
+    }
+}
