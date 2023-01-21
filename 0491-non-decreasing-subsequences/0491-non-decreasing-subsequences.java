1abf2ce17d4527bf9c3d072c1c1f0612ef523e83
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-01-20 22:37:55 -0500

    Time: 6 ms (87.48%), Space: 48.1 MB (87.85%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,23 +1,26 @@
 class Solution {
     public List<List<Integer>> findSubsequences(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
         List<Integer> perm = new ArrayList<>();
         generateSubseq(0,nums,perm,result);
-        return new ArrayList<>(result);
+        return result;
     }
     
-    public void generateSubseq(int index, int[] nums, List<Integer> perm, Set<List<Integer>> result){
-        if(index==nums.length){
-            if(perm.size()>=2){
-                result.add(new ArrayList<>(perm));
+    public void generateSubseq(int index, int[] nums, List<Integer> perm, List<List<Integer>> result){
+        if(perm.size()>=2){
+            result.add(new ArrayList<>(perm));
+        }
+        Set<Integer> used = new HashSet<>();
+        for(int i = index; i<nums.length; i++){
+            if(used.contains(nums[i])){
+                continue;
             }
-            return;
+            if(perm.size()==0 || perm.get(perm.size()-1)<=nums[i]){
+                perm.add(nums[i]);
+                used.add(nums[i]);
+                generateSubseq(i+1,nums,perm,result);
+                perm.remove(perm.size()-1);
+            }
         }
-        if(perm.size()==0 || perm.get(perm.size()-1)<=nums[index]){
-            perm.add(nums[index]);
-            generateSubseq(index+1,nums,perm,result);
-            perm.remove(perm.size()-1);
-        }
-        generateSubseq(index+1,nums,perm,result);
     }
 }
