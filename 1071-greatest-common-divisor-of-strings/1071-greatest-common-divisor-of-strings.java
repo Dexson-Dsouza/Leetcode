c84a9b8b540acd361fc141e9879963eee7bade55
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-02-01 08:12:26 -0500

    Time: 42 ms (7.48%), Space: 42.6 MB (38.58%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,35 +1,21 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        String smallestStr = "";
-        int l1 = str1.length();
-        int l2 = str2.length();
-        if(l1<l2){
-            smallestStr = str1;
-        }else{
-            smallestStr = str2;
-        }
-        
-        int len = smallestStr.length();
-        
-        for(int i=len;i>=0;i--){
-            int curLen = i + 1;
-            if(l1%curLen == 0  && l2%curLen == 0){
-                String curString = smallestStr.substring(0,i+1);
-                StringBuilder temp1 = new StringBuilder();
-                while(temp1.length()!=l1){
-                    temp1.append(curString);
-                }
-                
-                StringBuilder temp2 = new StringBuilder();
-                while(temp2.length()!=l2){
-                    temp2.append(curString);
-                }
-                
-                if(temp1.toString().equals(str1) && temp2.toString().equals(str2)){
-                    return curString;
-                }
+        StringBuilder t=new StringBuilder();
+        String res="";
+        for(char c:str2.toCharArray()){
+            t.append(c);
+            StringBuilder t1=new StringBuilder();
+            while(t1.length()<str1.length()){
+                t1.append(t.toString());
+            }
+            StringBuilder t2=new StringBuilder();
+            while(t2.length()<str2.length()){
+                t2.append(t.toString());
+            }
+            if(t1.toString().equals(str1) && t2.toString().equals(str2)){
+                res = t.toString();
             }
         }
-        return "";
+        return res;
     }
 }
