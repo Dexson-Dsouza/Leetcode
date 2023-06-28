3fc32e0023710a9dbccd83dc9adc1129cde7ab33
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-06-28 07:24:01 -0400

    Time: 40 ms (76.55%), Space: 57.2 MB (24.21%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,45 +1,46 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        Map<Integer,List<double[]>> adj=new HashMap<>();
         
-        List<List<double[]>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
+        for(int node=0;node<n;node++){
+            adj.put(node,new ArrayList<>());
         }
         
-        for(int i=0;i<edges.length;i++){
-            int from = edges[i][0];
-            int to = edges[i][1];
-            double prob = succProb[i];
+        int totalEdges=edges.length;
+        
+        for(int edge=0;edge<totalEdges;edge++){
+            int from=edges[edge][0];
+            int to=edges[edge][1];
+            double prob=succProb[edge];
             adj.get(from).add(new double[]{to,prob});
             adj.get(to).add(new double[]{from,prob});
         }
         
-        PriorityQueue<double[]> pq=new PriorityQueue<>((a,b)->Double.compare(b[0],a[0]));
+        PriorityQueue<double[]> processingEdge=new PriorityQueue<double[]>(
+            (a,b)->Double.compare(b[1],a[1]));
         
-        Set<Integer> visited=new HashSet<>();
-        pq.add(new double[]{1,start});
+        double[] probs=new double[n];
+        processingEdge.add(new double[]{start,1});
         
-        while(pq.size()>0){
-            double prob = pq.peek()[0];
-            int node = (int)pq.peek()[1];
-            pq.poll();
-            visited.add(node);
-            // System.out.println(node+" "+prob);
+        while(processingEdge.size()>0){
+            double[] top=processingEdge.poll();
+            int node=(int)(top[0]);
+            double prob=top[1];
             if(node==end){
                 return prob;
             }
             
-            for(double[] neighbourProbArr : adj.get((int)node)){
-                int neighbour=(int)neighbourProbArr[0];
-                double curProb = neighbourProbArr[1];
-                if(visited.contains(neighbour)==false){
-                    
-                    pq.add(new double[]{curProb*prob,neighbour});
-                    // System.out.println(neighbour+"---------"+curProb*prob);
+            for(double[] adjecentEdges:adj.get(node)){
+                int neighbour= (int)(adjecentEdges[0]);
+                double neighbourProb= adjecentEdges[1];
+                
+                if(neighbourProb*prob> probs[neighbour]){
+                    probs[neighbour]=neighbourProb*prob;
+                    processingEdge.add(new double[]{neighbour,neighbourProb*prob});
                 }
             }
+            
         }
-        
         return 0;
     }
 }
