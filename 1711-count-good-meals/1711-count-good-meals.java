4c320da1f80d7565f1939efacb736b719a7bf567
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-09-28 22:22:33 -0400

    Time: 415 ms (8.17%), Space: 56.7 MB (24.04%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPairs(int[] deliciousness) {
+        Set<Integer> powersOf2 = new HashSet<>();
+        int mod = (int)Math.pow(10,9)+7;
+        long curNum = 1;
+        powersOf2.add(1);
+        for(int i=1;i<=32;i++){
+            curNum = curNum<<1;
+            powersOf2.add((int)(curNum%mod));
+        }
+        Map<Integer,Integer> s = new HashMap<>();
+        long count = 0;
+        for(int i=0;i<deliciousness.length;i++){
+            int cur = deliciousness[i];
+            for(int power:powersOf2){
+                if(s.containsKey(power-cur)){
+                    count+=s.get(power-cur);
+                }
+            }
+            s.put(cur,s.getOrDefault(cur,0)+1);
+        }
+        return (int)(count%mod);
+    }
+}
