b7cea62c7c6081397e79f72bd7b35f3fe1fbd920
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-11-14 12:51:27 -0500

    Time: 210 ms (48.04%), Space: 44.9 MB (35.54%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -8,7 +8,7 @@ class Solution {
             rightCount[cur-'a']++;
         }
         
-        Set<String> palindromes = new HashSet<>();
+        Set<Integer> palindromes = new HashSet<>();
         
         for(int i=0;i<s.length();i++){
             char cur= s.charAt(i);
@@ -16,9 +16,9 @@ class Solution {
             rightCount[cur-'a']--;
             for(int characterIndex=0;characterIndex<26;characterIndex++){
                 int palCount = Math.min(leftCount[characterIndex],rightCount[characterIndex]);
-                String key = characterIndex+"_"+curIndex+"_"+characterIndex;
+                int key = 26*cur + characterIndex;
                 if(palCount>0 && palindromes.contains(key)==false){
-                    palindromes.add(characterIndex+"_"+curIndex+"_"+characterIndex);
+                    palindromes.add(key);
                 }
             }
             leftCount[cur-'a']++;
