4cdf42aef472c5ebd89b9fc5e07267d55cfc0cb0
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-11-29 10:50:47 -0500

    Time: 3 ms (6.24%), Space: 42.1 MB (44.06%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,33 +1,44 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int count[]=new int[101];
-        
-        int i=nums.length-1;
-        while(i>=0){
-            int cur=nums[i];
-            count[cur]++;
-            boolean found=false;
-            for(int next=cur+1;next<=100;next++){
-                if(count[next]>0){
-                    count[next]--;
-                    nums[i]=next;
-                    found=true;
-                    break;
-                }
-            }
-            if(found){
-                break;
-            }
-            i--;
+    public static void nextPermutation(int[] nums) {
+      TreeMap<Integer,Integer> visitedDigits = new TreeMap<>();
+
+      int index = nums.length-1;
+      boolean foundReplacement = false;
+      while(index>=0 && !foundReplacement){
+        int curDigit = nums[index];
+        Integer greaterDigit = visitedDigits.higherKey(curDigit);
+        visitedDigits.put(curDigit,visitedDigits.getOrDefault(curDigit, 0)+1);
+      
+        // System.out.println(curDigit+" "+greaterDigit);
+        if(greaterDigit!=null){
+          foundReplacement=true;
+          nums[index]=greaterDigit;
+          visitedDigits.put(greaterDigit,visitedDigits.get(greaterDigit)-1);
+        }else{
+          index--;
         }
-        i++;
-        for(int cur=0;cur<=100;cur++){
-            while(count[cur]>0){
-                count[cur]--;
-                nums[i]=cur;
-                i++;
-            }
+      }
+
+      if(foundReplacement){
+        //add remaining digits in sorted order
+        int i = index+1;
+        for(int digit:visitedDigits.keySet()){
+          int digitCount = visitedDigits.get(digit);
+          // System.out.println(digit+" "+digitCount+" "+i);
+          while(digitCount>0){
+            nums[i]=digit;
+            i++;
+            digitCount--;
+          }
         }
-        
+
+      }else{
+        //reverse
+        int[] clonedNums = nums.clone();
+        for(int i=0;i<clonedNums.length;i++){
+          nums[nums.length-1-i] = clonedNums[i]; 
+        }
+      }
+
     }
 }
