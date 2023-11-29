069683221f94ebea003ce0e1e3f3b2f7ebc01edc
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-11-29 16:10:09 -0500

    Time: 8 ms (68.81%), Space: 44.8 MB (20.50%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -1,19 +1,8 @@
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people,(a,b)->a[0]!=b[0]?a[0]-b[0]:b[1]-a[1]);
-        
-        for(int i=people.length-1;i>=0;i--){
-            int steps=people[i][1];
-            
-            int index=i;
-            while(steps>0){
-                int[] temp = people[index];
-                people[index] = people[index+1];
-                people[index+1]=temp;
-                steps--;
-                index++;
-            }
-        }
-        return people;
+        Arrays.sort(people,(a,b)-> a[0]==b[0]?a[1]-b[1]:b[0]-a[0]);
+        List<int[]> ordered = new LinkedList<>();
+	    for (int[] p: people) ordered.add(p[1], p);
+        return ordered.toArray(new int[people.length][2]);
     }
 }
