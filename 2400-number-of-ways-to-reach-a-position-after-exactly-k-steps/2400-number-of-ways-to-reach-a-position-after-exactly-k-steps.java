d764fa1e3f86bd1ecd0efec9c1751194a024ab2d
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-11-30 10:45:58 -0500

    Time: 240 ms (22.78%), Space: 71.2 MB (53.25%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.java
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -2,18 +2,20 @@ class Solution {
     private int mod = (int)(Math.pow(10,9)+7);
     
     public int numberOfWays(int startPos, int endPos, int k) {
-        Map<String,Integer> positionStepMap=new HashMap<>();
+        Map<Integer,Map<Integer,Integer>> positionStepMap=new HashMap<>();
         return findAllWays(startPos,endPos,k,positionStepMap);
     }
     
-    public int findAllWays(int startPos, int endPos, int k, Map<String,Integer> positionStepMap){
+    public int findAllWays(int startPos, int endPos, int k, Map<Integer,
+                           Map<Integer,Integer>> positionStepMap){
         if(k==0){
             return startPos==endPos?1:0;
         }
         
-        String positionKey = startPos+"_"+k;
-        if(positionStepMap.containsKey(positionKey)){
-            return positionStepMap.get(positionKey);
+        // String positionKey = startPos+"_"+k;
+        if(positionStepMap.containsKey(startPos) &&
+           positionStepMap.get(startPos).containsKey(k)){
+            return positionStepMap.get(startPos).get(k);
         }
         
         int steps=0;
@@ -23,7 +25,11 @@ class Solution {
             int takeRight = findAllWays(startPos+1,endPos,k-1,positionStepMap);
             steps=(takeLeft + takeRight)%mod;
         }
-        positionStepMap.put(positionKey,steps);
+        if(!positionStepMap.containsKey(startPos)){
+            positionStepMap.put(startPos,new HashMap<>());
+        }
+        
+        positionStepMap.get(startPos).put(k,steps);
         return steps;
     }
 }
