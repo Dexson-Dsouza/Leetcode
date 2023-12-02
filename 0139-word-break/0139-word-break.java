c19cb7fee22fa65f5d96a855c393c49ff86231b8
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-12-02 15:12:47 -0500

    Time: 3 ms (83.98%), Space: 40.9 MB (87.99%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,21 +1,22 @@
-public class Solution {
+class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordDictSet = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0]=true;
         for (int i = 1; i <= s.length(); i++) {
-            if(dp[i-1]==false){
-                continue;
-            }
-            for(String word:wordDictSet){
-                int len = word.length();
-                int startIndex = i-1;
-                int endIndex = startIndex+len;
-                if(endIndex<=s.length() && s.substring(startIndex,endIndex).equals(word)){
-                    dp[endIndex]=true;
+            for (String word: wordDict) {
+                // Handle out of bounds case
+                if (i < word.length()) {
+                    continue;
                 }
+                
+                if (dp[i - word.length()] 
+                    && s.substring(i - word.length(), i ).equals(word) ){
+                    dp[i] = true;   
+                    break;
+                 }
             }
         }
+        
         return dp[s.length()];
     }
 }
