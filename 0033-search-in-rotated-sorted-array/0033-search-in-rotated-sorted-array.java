8eabeb29a0784c8cc9bcbde1f10abd200f7cfdfc
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-12-01 20:21:41 -0500

    Time: 0 ms (100.00%), Space: 40.8 MB (53.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,26 +1,34 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
+    public static int search(int[] nums, int target) {
+    int l = 0;
+    int r = nums.length - 1;
+    int index = -1;
+    while (l <= r) {
         
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[left]<=nums[mid]){
-                if(target>=nums[left] && target<nums[mid]){
-                    right=mid-1;
-                }else{
-                    left=mid+1;
-                }
-            }else{
-                if(target>nums[mid] && target<=nums[right]){
-                    left=mid+1;
-                }else{
-                    right=mid-1;
-                }
-            }
+      int mid = l + (r - l) / 2;
+        // System.out.println(l+" "+r +" "+mid);
+      if (nums[l] <= nums[mid]) {
+        if (nums[l] <= target && nums[mid] >= target) {
+          r = mid - 1;
+          if (nums[mid] == target) {
+            index = mid;
+            break;
+          }
+        } else {
+          l = mid + 1;
         }
-        return -1;
+      } else {
+        if (nums[mid] <= target && nums[r] >= target) {
+          l = mid + 1;
+          if (nums[mid] == target) {
+            index = mid;
+            break;
+          }
+        } else {
+          r = mid - 1;
+        }
+      }
     }
+    return index;
+  }
 }
