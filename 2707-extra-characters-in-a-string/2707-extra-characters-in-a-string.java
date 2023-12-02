fb96f8be77a6fcc9f1470bdfd60603efed92bfcd
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-12-02 16:10:14 -0500

    Time: 43 ms (49.36%), Space: 44.6 MB (12.53%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,33 +1,34 @@
 class Solution {
     public int minExtraChar(String s, String[] dictionary) {
-    Map<String, Integer> processedWords = new HashMap<>();
+    Map<Integer, Integer> processedWords = new HashMap<>();
     return findMinExtra(
+        0,
         s,
         new HashSet<String>(Arrays.asList(dictionary)),
         processedWords);
   }
 
-  public int findMinExtra(String s, HashSet<String> dictionary, Map<String, Integer> processedWords) {
-    if (s.length() == 0) {
+  public int findMinExtra(int index,String s, HashSet<String> dictionary, Map<Integer, Integer> processedWords) {
+    if (index == s.length()) {
       return 0;
     }
 
-    if (processedWords.containsKey(s)) {
-      return processedWords.get(s);
+    if (processedWords.containsKey(index)) {
+      return processedWords.get(index);
     }
 
     int count = Integer.MAX_VALUE;
 
     // skip this character
-    count = Math.min(count, 1 + findMinExtra(s.substring(1), dictionary, processedWords));
+    count = Math.min(count, 1 + findMinExtra(index + 1,s, dictionary, processedWords));
 
     // take this character
-    for (int i = 0; i < s.length(); i++) {
-      if (dictionary.contains(s.substring(0, i + 1))) {
-        count = Math.min(count, findMinExtra(s.substring(i + 1), dictionary, processedWords));
+    for (int i = index; i < s.length(); i++) {
+      if (dictionary.contains(s.substring(index, i + 1))) {
+        count = Math.min(count, findMinExtra(i + 1,s, dictionary, processedWords));
       }
     }
-    processedWords.put(s, count);
+    processedWords.put(index, count);
     return count;
   }
 }
