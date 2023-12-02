083c4bd312c002649373b49674b4d1a740679a6f
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-12-01 23:41:11 -0500

    Time: 4 ms (38.03%), Space: 44.4 MB (24.93%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,25 +1,30 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        backtrack(nums,res,new ArrayList<>(),new boolean[nums.length]);
-        return res;
-    }
-    public void backtrack(int[]nums,List<List<Integer>>res,List<Integer> list,boolean[]used){
-        if(list.size()==nums.length){
-            res.add(new ArrayList<>(list));
-            return;
+        Map<Integer, Integer> digitMap = new HashMap<>();
+        for (int n : nums) {
+          digitMap.put(n, digitMap.getOrDefault(n, 0) + 1);
         }
-        else{
-            for(int i=0;i<nums.length;i++){
-                if(used[i]||(i>0&&nums[i]==nums[i-1] && !used[i-1]))
-                    continue;
-                used[i]=true;
-                list.add(nums[i]);
-                backtrack(nums,res,list,used);
-                used[i]=false;
-                list.remove(list.size()-1);
-            }
+        List<Integer> currentPerm = new ArrayList<>();
+        List<List<Integer>> permutations = new ArrayList<>();
+        generatePerm(nums.length,digitMap, currentPerm, permutations);
+        return permutations;
+      }
+
+      void generatePerm(int total, Map<Integer, Integer> digitMap, List<Integer> currentPerm, List<List<Integer>> permutations) {
+        if (total == currentPerm.size()) {
+          permutations.add(new ArrayList<>(currentPerm));
+          return;
         }
-    }
+
+        for(int n:digitMap.keySet()){
+          if(digitMap.get(n)==0){
+            continue;
+          }
+          currentPerm.add(n);
+          digitMap.put(n,digitMap.get(n)-1);
+          generatePerm(total,digitMap, currentPerm, permutations);
+          currentPerm.remove(currentPerm.size()-1);
+          digitMap.put(n,digitMap.get(n)+1);
+        }
+      }
 }
