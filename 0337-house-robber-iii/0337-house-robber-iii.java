5c7d6e8f65cbdeefe727362e06eec6b641c0358d
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-12-02 20:59:50 -0500

    Time: 0 ms (100.00%), Space: 44 MB (28.87%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] robSkipPair = steal(root);
+        return Math.max(robSkipPair[0],robSkipPair[1]);
+    }
+    
+    int[] steal(TreeNode root){
+        if(root==null){
+            return new int[]{0,0};
+        }
+        
+        int[] leftPair = steal(root.left);
+        int[] rightPair = steal(root.right);
+        
+        int stealChildrenAmount = leftPair[0]+rightPair[0];
+        int skipChildrenAmount = leftPair[1]+rightPair[1];
+        int currentStealAmount = root.val + skipChildrenAmount;
+        
+        int maxSteal = Math.max(currentStealAmount,stealChildrenAmount);
+        
+        return new int[]{maxSteal,stealChildrenAmount};
+        
+    }
+}
