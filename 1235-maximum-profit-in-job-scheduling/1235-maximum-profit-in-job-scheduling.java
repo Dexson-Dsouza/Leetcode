6108f1d0bf85916e042148a3497d6c567486aadf
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2023-12-04 14:20:47 -0500

    Time: 102 ms (16.29%), Space: 55 MB (46.30%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,54 +1,46 @@
-public class Job{
-    int startTime;
-    int endTime;
-    int profit;
-    Job(int startTime, int endTime, int profit){
-        this.startTime=startTime;
-        this.endTime=endTime;
-        this.profit=profit;
-    }
-}
+
 class Solution {
+    public class Job{
+        int startTime;
+        int endTime;
+        int profit;
+        Job(int startTime, int endTime, int profit){
+            this.startTime=startTime;
+            this.endTime=endTime;
+            this.profit=profit;
+        }
+    }
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         int totalJobs = startTime.length;
-        int[] bestPrevIndex=new int[totalJobs];
-        
-        List<Job> jobList=new ArrayList<>();
-        for(int i=0;i<totalJobs;i++){
-            jobList.add(new Job(startTime[i],endTime[i],profit[i]));
+
+        List<Job> jobList = new ArrayList<>();
+        for (int i = 0; i < totalJobs; i++) {
+          jobList.add(new Job(startTime[i], endTime[i], profit[i]));
         }
-        
-        jobList.sort((a,b)->(a.endTime-b.endTime));
-        
-        for(int i=0;i<totalJobs;i++){
-            int left = 0;
-            int right = i-1;
-            int curStart = jobList.get(i).startTime;
-            int curEnd = jobList.get(i).endTime;
-            int prevIndex=-1;
-            while(left<=right){
-                int mid = (left+right)/2;
-                int midEndTime = jobList.get(mid).endTime;
-                if(midEndTime<=curStart){
-                    prevIndex=mid;
-                    left=mid+1;
-                }else{
-                    right=mid-1;
-                }
-            }
-            bestPrevIndex[i]=prevIndex;
+
+        jobList.sort((a, b) -> (a.endTime - b.endTime));
+
+        TreeMap<Integer, Integer> timeProfitMap = new TreeMap<>();
+        int maxProfit = 0;
+        for (int i = 0; i < totalJobs; i++) {
+          int curProfit = jobList.get(i).profit;
+          int curStartTime = jobList.get(i).startTime;
+          int curEndTime = jobList.get(i).endTime;
+
+          Integer prevBestJobEndTime = timeProfitMap.floorKey(curStartTime);
+          if (prevBestJobEndTime != null) {
+            curProfit += timeProfitMap.get(prevBestJobEndTime);
+          }
+
+          int prevProfit = 0;
+          Integer prevCompletedJobEndTime = timeProfitMap.floorKey(curEndTime);
+          if (prevCompletedJobEndTime != null) {
+            prevProfit = timeProfitMap.get(prevCompletedJobEndTime);
+          }
+          int curMaxProfit = Math.max(curProfit, prevProfit);
+          timeProfitMap.put(curEndTime, curMaxProfit);
+          maxProfit = Math.max(maxProfit, curMaxProfit);
         }
-        int result=0;
-        int[] maxProfitArr=new int[totalJobs];
-        for(int i=0;i<totalJobs;i++){
-            int prevProfit = i>0? maxProfitArr[i-1]:0;
-            int prevIndex=bestPrevIndex[i];
-            int prevPossibleProfit = prevIndex == -1? 0 :  maxProfitArr[prevIndex];
-            int currentProfit = jobList.get(i).profit;
-            
-            maxProfitArr[i] = Math.max(prevProfit ,  currentProfit + prevPossibleProfit);
-            result=Math.max(result,maxProfitArr[i]);
-        }
-        return result;
-    }
+        return maxProfit;
+      }
 }
