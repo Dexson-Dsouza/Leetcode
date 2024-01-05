4a40aeaf6f1262922fcc3179a099dfdbee78ff8f
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-04 20:28:19 -0500

    Time: 2 ms (69.94%), Space: 45.2 MB (19.40%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,14 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
+        int maxDist = 0;
+        int index = 0;
         int curEnd = 0;
-        int maxJump = 0;
         int steps = 0;
-        for(int i = 0; i<nums.length-1; i++){
-            maxJump = Math.max(maxJump, i + nums[i]);
-            if(curEnd == i){
+        while(index<nums.length && curEnd<nums.length-1){
+            maxDist = Math.max(maxDist,nums[index] + index);
+            if(curEnd==index){
+                curEnd = maxDist;
                 steps++;
-                curEnd = maxJump;
             }
+            index++;
         }
         return steps;
     }
