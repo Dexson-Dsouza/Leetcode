6ce7b2ba78e117d73a9b18cff89323e0dea29fa2
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-07 00:22:02 -0500

    Time: 56 ms (84.44%), Space: 47 MB (89.75%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,35 +1,35 @@
 class DetectSquares {
-    Map<Integer,Map<Integer,Integer>> map;
+    Map<Integer,Map<Integer,Integer>> xMap;
     public DetectSquares() {
-        map=new HashMap<>();
+        xMap = new HashMap<>();
     }
     
     public void add(int[] point) {
-        if(map.containsKey(point[0])==false){
-            map.put(point[0],new HashMap<>());
+        if(!xMap.containsKey(point[0])){
+            xMap.put(point[0],new HashMap<>());
         }
-        Map<Integer,Integer> Ymap=map.get(point[0]);
-        Ymap.put(point[1],Ymap.getOrDefault(point[1],0)+1);
+        Map<Integer,Integer> yCountMap = xMap.get(point[0]);
+        yCountMap.put(point[1],yCountMap.getOrDefault(point[1],0)+1);
     }
     
     public int count(int[] point) {
-        if(map.containsKey(point[0])==false){
+        int x1=point[0],y1=point[1];
+        int count = 0;
+        
+        if(xMap.containsKey(x1)==false){
             return 0;
         }
-        int x1=point[0];
-        int y1=point[1];
-        int count=0;
         
-        // System.out.println(x1+" "+y1);
-        for(int x2:map.keySet()){
-            
-            //check for point on same x
-            if(x2!=x1  && map.get(x2).containsKey(y1)){
-            // System.out.println(" contains x2");
-                for(int y2:map.get(x2).keySet()){
-                    if(y1!=y2 && map.get(x1).containsKey(y2) && Math.abs(x1-x2)==Math.abs(y1-y2)){
-            // System.out.println(" contains y2");
-                        count += map.get(x1).get(y2)*map.get(x2).get(y1)*map.get(x2).get(y2); 
+        Map<Integer,Integer> source_y_count_map = xMap.get(x1);
+        
+        for(int x2:xMap.keySet()){
+            if(x2!=x1 && xMap.get(x2).containsKey(y1)){
+                Map<Integer,Integer> target_y_count_map = xMap.get(x2);
+                for(int y2:target_y_count_map.keySet()){
+                    if(y2!=y1 && source_y_count_map.containsKey(y2)
+                       && Math.abs(x1-x2)==Math.abs(y1-y2)){
+                        count +=
+                            source_y_count_map.get(y2)*xMap.get(x2).get(y1)*xMap.get(x2).get(y2);
                     }
                 }
             }
