678927a4e8c1cd8570a763d783612205104e794d
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-06 20:21:00 -0500

    Time: 164 ms (25.61%), Space: 64.4 MB (5.04%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,37 +1,42 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
-        Map<Integer,Integer> count=new HashMap<>();
-        for(int n:changed){
-            count.put(n,count.getOrDefault(n,0)+1);
+        
+        if(changed.length%2 == 1){
+            return new int[]{};
         }
+        
+        Map<Integer,Integer> countMap = new HashMap<>();
+        
+        for(int n:changed){
+            countMap.put(n,countMap.getOrDefault(n,0)+1);
+        }
+        
+        int originalArrLen=changed.length/2;
         
         Arrays.sort(changed);
-        
-        
-        int[] originalArr= new int [changed.length/2];
-        int index=0;
-        
+        List<Integer> resultList = new ArrayList<>();
         for(int n:changed){
-            if(count.get(n)==0){
-                continue;
+            if(countMap.get(n)>0){
+                int doubledVal = n*2;
+                if((n!=0 && countMap.getOrDefault(doubledVal,0)>0) ||
+                   (countMap.getOrDefault(doubledVal,0)>1)){
+                    // update count and result
+                    resultList.add(n);
+                    countMap.put(n,countMap.get(n)-1);
+                    countMap.put(doubledVal,countMap.get(doubledVal)-1);
+                }
             }
-            int next=2*n;
-            if(next==n && count.get(n)<2){
-                return new int[0];
-            }
-            if(count.getOrDefault(next,0)==0){
-                return new int[0];
-            }
-            originalArr[index++]=n;
-            count.put(n,count.getOrDefault(n,0)-1);
-            count.put(next,count.getOrDefault(next,0)-1);
         }
         
-        for(int n:count.keySet()){
-            if(count.get(n)!=0){
-                return new int[0];
-            }
+        if(changed.length/2 != resultList.size()){
+            return new int[]{};
         }
-        return originalArr;
+        
+        int[] res=new int[resultList.size()];
+        for(int i=0;i<resultList.size();i++){
+            res[i]=resultList.get(i);
+        }
+        return res;
+        
     }
 }
