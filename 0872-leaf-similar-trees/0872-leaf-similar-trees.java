e81a8d7c6262a4f37cc0089aacb5b1f62b134261
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-08 20:52:38 -0500

    Time: 0 ms (100.00%), Space: 41.2 MB (34.78%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -15,18 +15,23 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        Queue<Integer> values1=new LinkedList<>();
-        Queue<Integer> values2=new LinkedList<>();
+        List<Integer> values1=new LinkedList<>();
+        List<Integer> values2=new LinkedList<>();
         getLeafs(root1,values1);
         getLeafs(root2,values2);
-        while(values1.size()>0 && values2.size()>0 && values1.peek() == values2.peek()){
-            values1.poll();
-            values2.poll();
+        if(values1.size()!=values2.size()){
+            return false;
         }
-        return values1.size()==0 && values2.size()==0;
+        
+        for(int i=0;i<values1.size();i++){
+            if(values1.get(i)!=values2.get(i)){
+                return false;
+            }
+        }
+        return true;
     }
     
-    public void getLeafs(TreeNode root, Queue<Integer> values){
+    public void getLeafs(TreeNode root, List<Integer> values){
         if(root==null){
             return;
         }
