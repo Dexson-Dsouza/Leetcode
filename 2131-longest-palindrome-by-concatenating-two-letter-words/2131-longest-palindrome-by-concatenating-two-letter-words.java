e0612ef13b7dc91949492048fea1ec8ea7ddd70c
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-10 14:23:26 -0500

    Time: 62 ms (65.91%), Space: 57 MB (76.18%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -9,43 +9,36 @@ class Solution {
         for(int i=0;i<wordCount;i++){
             count.put(words[i],count.getOrDefault(words[i],0)+1);
         }
-        // System.out.println(count);
+        
+        Set<String> processed=new HashSet<>();
+        System.out.println(count);
         for(String word:count.keySet()){
             String reverse= new StringBuilder(word).reverse().toString();
-            // System.out.println(word + " "+ reverse+" "+longestLen);
+            
+            if(processed.contains(word) || processed.contains(reverse)){
+                continue;
+            }
+            processed.add(word);
+            processed.add(reverse);
             if(word.equals(reverse)){
                 int totalCount = count.get(word);
-                int maxTaken = totalCount/2;
-                longestLen += maxTaken*(word.length()*2);
-                int remaining = totalCount - maxTaken*2;
-                if(remaining>0 && singlePalTaken==false){
+                int maxTaken = totalCount - totalCount%2;
+                longestLen += maxTaken*2;
+                if(totalCount%2==1 && singlePalTaken==false){
                     singlePalTaken=true;
                     longestLen+=2;
-                    remaining--;
                 }
-                count.put(word,remaining);
             }else{
                 int count1=count.getOrDefault(word,0);
                 int count2=count.getOrDefault(reverse,0);
-                
                 int maxTaken= Math.min(count1,count2);
-                
                 if(maxTaken>0){
-                    count.put(word,count.get(word)-maxTaken);
-                    count.put(reverse,count.get(reverse)-maxTaken);
-                    longestLen += maxTaken*(word.length() + reverse.length());
+                    longestLen += maxTaken*(4);
                 }
             }
-            
-            
-            
+            // System.out.println(word+" "+reverse+" "+longestLen);
         }
         
         return longestLen;
-    }
-    
-    public boolean isPal(String s1){
-        StringBuilder s2=new StringBuilder(s1);
-        return s2.reverse().toString().equals(s1);
     }
 }
