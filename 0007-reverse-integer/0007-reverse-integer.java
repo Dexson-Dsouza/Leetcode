54f2c2f4acb3366e730858ce31f700d2d583a877
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-13 15:28:34 -0500

    Time: 1 ms (94.85%), Space: 40.7 MB (37.67%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -4,17 +4,17 @@ class Solution {
         
         while(x!=0){
             int lastDigit = x%10;
-            x/=10;
             
             // checks for positive num
-            if(lastDigit>0 &&  rev  > (Integer.MAX_VALUE-lastDigit)/10){
+            if(x>0 &&  rev  > (Integer.MAX_VALUE-lastDigit)/10){
                 return 0;
             }
             // checks for negative num
-            if(lastDigit<0 && rev < (Integer.MIN_VALUE-lastDigit)/10 ){
+            if(x<0 && rev < (Integer.MIN_VALUE-lastDigit)/10 ){
                 return 0;
             }
             
+            x/=10;
             rev = rev *10 + lastDigit;
         }
         
