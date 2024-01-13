6ffdfe521de159d20a15577b2a4d7a795923170a
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-13 15:30:33 -0500

    Time: 5 ms (79.73%), Space: 43.8 MB (42.42%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        return x>=0 && x == reverse(x);
+    }
+    
+    public int reverse(int x) {
+        int rev = 0;
+        
+        while(x!=0){
+            int lastDigit = x%10;
+            
+            // checks for positive num
+            if(x>0 &&  rev  > (Integer.MAX_VALUE-lastDigit)/10){
+                return 0;
+            }
+            // checks for negative num
+            if(x<0 && rev < (Integer.MIN_VALUE-lastDigit)/10 ){
+                return 0;
+            }
+            
+            x/=10;
+            rev = rev *10 + lastDigit;
+        }
+        
+        return rev;
+    }
+}
