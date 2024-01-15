47c952c2b8d51fed2904c842241a2e0adb594923
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-14 20:26:10 -0500

    Time: 5 ms (85.64%), Space: 49.8 MB (7.05%) - LeetHub

--- 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.java
+++ 0470-implement-rand10-using-rand7/0470-implement-rand10-using-rand7.java
@@ -5,11 +5,10 @@
  */
 class Solution extends SolBase {
     public int rand10() {
-        int r = 40;
-        while(r>=40){
-            r = (rand7() - 1)*7 + rand7()-1;
+        int sum = (rand7()-1)*7 + (rand7()-1);
+        while(sum>=40){
+            sum = (rand7()-1)*7+ (rand7()-1);
         }
-        
-        return r%10+1;
+        return (sum%10)+1;
     }
 }
