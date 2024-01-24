0be31e972dfd95a5c2a48a373bc017cb2a1925fc
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-01-24 10:24:18 -0500

    Time: 16 ms (49.11%), Space: 69.9 MB (59.07%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -15,30 +15,40 @@
  */
 class Solution {
     public int pseudoPalindromicPaths (TreeNode root) {
-        int[] countOfPalindromicPath=new int[1];
-        findPaths(root,new int[10],countOfPalindromicPath);
-        return countOfPalindromicPath[0];
+        int[] count=new int[10];
+        return find_pal_paths(root,count);
     }
     
-    void findPaths(TreeNode root, int[] values, int[] countOfPalindromicPath){
+    public int find_pal_paths( TreeNode root, int[] count){
         if(root==null){
-            return;
+            return 1;
         }
-        values[root.val-1]++;
+        
+        count[root.val]++;
         if(root.left==null && root.right==null){
-            int countOfOdds=0;
-            for(int i=0;i<10;i++){
-                if(values[i]%2==1){
-                    countOfOdds++;
-                }
-            }
-            if(countOfOdds<=1){
-                countOfPalindromicPath[0]++;
-            }
-        }else{
-            findPaths(root.left,values,countOfPalindromicPath);
-            findPaths(root.right,values,countOfPalindromicPath);
+            int path_count=can_make_pal(count);
+            count[root.val]--;
+            return path_count;
         }
-        values[root.val-1]--;
+        int left_route_count = 0;
+        if(root.left!=null){
+            left_route_count = find_pal_paths(root.left,count);
+        }
+        int right_route_count = 0;
+        if(root.right!=null){
+            right_route_count = find_pal_paths(root.right,count);
+        }
+        count[root.val]--;
+        return left_route_count+right_route_count;
+    }
+    
+    public int can_make_pal(int[] count){
+        int odd_count=0;
+        for(int i=1;i<=9;i++){
+            if(count[i]%2==1){
+                odd_count++;
+            }
+        }
+        return odd_count>1?0:1;
     }
 }
