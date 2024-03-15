0de6b9bb06631977161aa69789e92a6c7ce4c357
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-03-14 20:47:49 -0400

    Time: 2 ms (78.65%), Space: 44.4 MB (62.16%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -2,43 +2,45 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         int rows=board.length;
         int cols=board[0].length;
+        
         for(int row=0;row<rows;row++){
-            int rowBit=0;
+            HashSet<Integer> rowSet = new HashSet<Integer>();
             for(int col=0;col<cols;col++){
                 if(board[row][col]!='.'){
                     int num=board[row][col]-'0';
-                    if((rowBit&(1<<num))!=0){
+                    if(rowSet.contains(num)){
                         return false;
                     }
-                    rowBit|=(1<<num);
+                    rowSet.add(num);
                 }
             }
         }
         
         for(int col=0;col<cols;col++){
-            int colBit=0;
+            HashSet<Integer> columnSet = new HashSet<Integer>();
             for(int row=0;row<rows;row++){
                 if(board[row][col]!='.'){
                     int num=board[row][col]-'0';
-                    if((colBit&(1<<num))!=0){
+                    if(columnSet.contains(num)){
                         return false;
                     }
-                    colBit|=(1<<num);
+                    columnSet.add(num);
                 }
             }
         }
         
+        
         for(int row=0;row<rows;row+=3){
             for(int col=0;col<cols;col+=3){
-                int blockBit=0;
+                HashSet<Integer> cubeSet = new HashSet<Integer>();
                 for(int i=row;i<row+3;i++){
                     for(int j=col;j<col+3;j++){
                        if(board[i][j]!='.'){
                             int num=board[i][j]-'0';
-                            if((blockBit&(1<<num))!=0){
+                            if(cubeSet.contains(num)){
                                 return false;
                             }
-                            blockBit|=(1<<num);
+                            cubeSet.add(num);
                         } 
                     }
                 }
