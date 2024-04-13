6a6fdd21a14d5cb396f7842ab2ff9086d9d610be
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-04-12 21:06:59 -0400

    Time: 74 ms (45.19%), Space: 58 MB (20.14%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,43 +1,19 @@
 class Solution {
-    public static int largestRectangleArea(int[] height) {
-        if (height.length == 0) {
-            return 0;
+    public int largestRectangleArea(int[] heights) {
+      int n = heights.length;
+      Stack<Integer> stack = new Stack();
+      int maxArea = 0;
+      for(int i = 0; i <= n; i++){
+        int h = i == n ? 0 : heights[i];
+        while(!stack.isEmpty() && h < heights[stack.peek()]){
+          int curHeight = heights[stack.pop()];
+          int prevIndex = stack.isEmpty() ? -1 : stack.peek();
+          int area = curHeight * (i - prevIndex - 1);
+          maxArea = Math.max(maxArea, area);
         }
-        int[] closestLessFromLeft = new int[height.length]; 
-        // index of the first bar the left that is lower than current
-        
-        int[] closestLessFromRight = new int[height.length]; 
-        // index of the first bar the right that is lower than current
-        
-        closestLessFromLeft[0] = -1;
-
-        for (int i = 1; i < height.length; i++) {
-            int closestIndex = i - 1;
-
-            while (closestIndex >= 0 && height[closestIndex] >= height[i]) {
-                closestIndex = closestLessFromLeft[closestIndex];
-            }
-            closestLessFromLeft[i] = closestIndex;
-        }
-        
-        
-        closestLessFromRight[height.length - 1] = height.length;
-
-        for (int i = height.length - 2; i >= 0; i--) {
-            int closestIndex = i + 1;
-
-            while (closestIndex < height.length && height[closestIndex] >= height[i]) {
-                closestIndex = closestLessFromRight[closestIndex];
-            }
-            closestLessFromRight[i] = closestIndex;
-        }
-
-        int maxArea = 0;
-        for (int i = 0; i < height.length; i++) {
-            maxArea = Math.max(maxArea, height[i] * 
-                               (closestLessFromRight[i] - closestLessFromLeft[i] - 1));
-        }
-
-        return maxArea;
+        stack.push(i);
+      }
+      return maxArea;  
     }
+
 }
