31dfde5da548bb6cf660488b93643b88a89beb4e
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-04-23 11:01:14 -0400

    Time: 111 ms (32.22%), Space: 57.8 MB (25.21%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,19 +1,39 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-      int n = heights.length;
-      Stack<Integer> stack = new Stack();
-      int maxArea = 0;
-      for(int i = 0; i <= n; i++){
-        int h = i == n ? 0 : heights[i];
-        while(!stack.isEmpty() && h < heights[stack.peek()]){
-          int curHeight = heights[stack.pop()];
-          int prevIndex = stack.isEmpty() ? -1 : stack.peek();
-          int area = curHeight * (i - prevIndex - 1);
-          maxArea = Math.max(maxArea, area);
+        int heightCount=heights.length;
+        int[] leftClosestPole = new int[heightCount];
+        int[] rightClosestPole = new int[heightCount];
+        
+        // use increasing sequences to get left and right closest and smaller poles
+        
+        Stack<Integer> leftPoles = new Stack<>();
+        for(int i=0;i<heightCount;i++){
+            int curHeight=heights[i];
+            while(leftPoles.size()>0 && heights[leftPoles.peek()]>=curHeight){
+                leftPoles.pop();
+            }
+            int leftClosestSmallerIndex = leftPoles.size()>0?leftPoles.peek()+1:0;
+            leftClosestPole[i]=leftClosestSmallerIndex;
+            leftPoles.add(i);
         }
-        stack.push(i);
-      }
-      return maxArea;  
+        
+        
+        Stack<Integer> rightPoles = new Stack<>();
+        for(int i=heightCount-1;i>=0;i--){
+            int curHeight=heights[i];
+            while(rightPoles.size()>0 && heights[rightPoles.peek()]>=curHeight){
+                rightPoles.pop();
+            }
+            int rightClosestSmallerIndex = rightPoles.size()>0?rightPoles.peek()-1:heightCount-1;
+            rightClosestPole[i]=rightClosestSmallerIndex;
+            rightPoles.add(i);
+        }
+        
+        int area=0;
+        for(int i=0;i<heightCount;i++){
+            area = Math.max(area, heights[i]*(rightClosestPole[i]-leftClosestPole[i]+1));
+        }
+        return area;
+        
     }
-
 }
