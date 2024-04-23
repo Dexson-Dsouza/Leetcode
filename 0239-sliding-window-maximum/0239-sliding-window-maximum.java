de52b070a0f5b88b777ea32eb2bac35aa6b54288
Dexson-Dsouza <74306132+Dexson-Dsouza@users.noreply.github.com>
2024-04-23 11:44:07 -0400

    Time: 30 ms (75.26%), Space: 58.3 MB (80.75%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,37 +1,25 @@
 class Solution {
-    public static int[] maxSlidingWindow(int[] nums, int k) {
-        Map<Integer,Integer> countMap = new HashMap<>();
-        PriorityQueue<Integer> maxWindowQueue = new PriorityQueue<>((a,b)->Integer.compare(b, a));
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums==null|| k<=0)
+            return new int[0];
+        int[]res=new int[nums.length-k+1];
+        Deque<Integer>dq=new ArrayDeque<>();
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+            while(!dq.isEmpty() && i-k>=dq.peekFirst())
+                dq.pollFirst();
+            
+            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i])
+                dq.pollLast();
+            
+            dq.addLast(i);
+            
+            if(i>=k-1){
+                res[idx]=nums[dq.peekFirst()];
+                idx++;
+            }
+        }
         
-        // fill queue with first window
-
-        for(int index=0;index<k-1;index++){
-          int num=nums[index];
-          maxWindowQueue.add(num);
-          countMap.put(num,countMap.getOrDefault(num,0)+1);
-        }
-
-        int[] maxInWindow = new int[nums.length-k+1];
-        int resultIndex = 0;
-        for(int index=k-1;index<nums.length;index++){
-          // pop old window member
-          int poppedIndex = index-k;
-          if(poppedIndex>=0){
-            int poppedNum = nums[poppedIndex];
-            countMap.put(poppedNum,countMap.get(poppedNum)-1);
-          }
-          //add new window member
-          int num = nums[index];
-          countMap.put(num,countMap.getOrDefault(num,0)+1);
-          maxWindowQueue.add(num);
-          // removed old element from queue
-          while(maxWindowQueue.size()>0 && countMap.get(maxWindowQueue.peek())==0){
-            maxWindowQueue.poll();
-          }
-          // System.out.println(maxWindowQueue);
-          maxInWindow[resultIndex] = maxWindowQueue.peek();
-          resultIndex++;
-        }
-      return maxInWindow;
+        return res;
     }
 }
